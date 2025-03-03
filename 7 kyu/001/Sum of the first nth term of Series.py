184686e87374708217b863261fffdaaa0ff0736d
Andrey Dmitriev <andrey.dmi3ev@gmail.com>
2025-03-03 22:41:37 +0300

    2025-03-03_2241

---
+++ 7 kyu/001/Sum of the first nth term of Series.py
@@ -0,0 +1,17 @@
+def series_sum(n):
+    result = 0
+    temp_list = [1]
+    if n > 1:
+        for item in range(1, n):
+            temp_list.append(temp_list[-1] + 3)
+        result = calc(temp_list)
+    else:
+        result = n
+    return r"{:.2f}".format(result)
+
+
+def calc(list):
+    result = 0
+    for item in list:
+        result += 1 / item
+    return round(result, 2)
