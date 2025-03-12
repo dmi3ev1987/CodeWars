7b31054862f2f960af10c47a746bf54fbc21257e
Andrey Dmitriev <andrey.dmi3ev@gmail.com>
2025-03-12 19:25:15 +0300

    2025-03-12_1925

---
+++ 6 kyu/001/Persistent Bugger.py
@@ -0,0 +1,21 @@
+def persistence(n):
+    count = 0
+    if len(str(n)) == 1:
+        return count
+    while len(str(n)) > 1:
+        digits_list = get_digits(n)
+        n = multiply_result(digits_list)
+        count += 1
+
+    return count
+
+
+def get_digits(digits_list):
+    return [int(digit) for digit in str(digits_list)]
+
+
+def multiply_result(input_list):
+    result = 1
+    for item in input_list:
+        result *= item
+    return result
