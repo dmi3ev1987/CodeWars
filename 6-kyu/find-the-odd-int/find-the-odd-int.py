73139d7342614dee8c1f9e93f6e7086dbe35f5bb
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-03-26 17:51:35 +0300

    Add solution - CodeHub

---
+++ 6-kyu/find-the-odd-int/find-the-odd-int.py
@@ -0,0 +1,21 @@
+def find_it(seq):
+    result_dict = {}
+    while len(seq) >= 1:
+        number, count, new_seq = count_number(seq)
+        result_dict[number] = count
+        seq = new_seq
+    return find_one_odd(result_dict)
+​
+​
+def count_number(seq):
+    number = seq[0]
+    count = seq.count(number)
+    result_seq = [item for item in seq if item != number]
+    return number, count, result_seq
+​
+​
+def find_one_odd(input_dict):
+    for key, value in input_dict.items():
+        if value % 2 != 0:
+            return key
+​
