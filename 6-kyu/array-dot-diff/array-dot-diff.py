37a4ef000774489eb6767a5c273348ded585a571
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-03-27 15:14:35 +0300

    Add solution - CodeHub

---
+++ 6-kyu/array-dot-diff/array-dot-diff.py
@@ -0,0 +1,12 @@
+def array_diff(a, b):
+    if not b:
+        return a
+    result_list = a
+    for item_b in b:
+        result_list = get_diff_list(result_list, item_b)
+    return result_list
+​
+​
+def get_diff_list(input_list, number):
+    return [item for item in input_list if item != number]
+​
