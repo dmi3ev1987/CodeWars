439847c1329d5013ebd03089d2e91eacf1ed730e
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-03-28 22:05:38 +0300

    Add solution - CodeHub

---
+++ 6-kyu/stop-gninnips-my-sdrow/stop-gninnips-my-sdrow.py
@@ -0,0 +1,7 @@
+def spin_words(sentence):
+    words_list = sentence.split()
+    result_sentence_list = [
+        word[::-1] if len(word) >= 5 else word for word in words_list
+    ]
+    return " ".join(result_sentence_list)
+​
