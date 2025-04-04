1bf25f25b05190d75fa81a5ff8218d2a1e036e42
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-04-04 14:26:43 +0300

    Add solution - CodeHub

---
+++ 6-kyu/bit-counting/bit-counting.py
@@ -0,0 +1,18 @@
+def count_bits(n):
+    count = 0
+    binary = to_binary(n)
+    for bit in binary:
+        if bit == "1":
+            count += 1
+    return count
+​
+​
+def to_binary(n):
+    if n == 0:
+        return "0"
+    binary = ""
+    while n > 0:
+        binary += str(n % 2)
+        n = n // 2
+    return binary
+​
