4d42511819b8685ac5dd281e791edf74e82ef0bb
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-04-08 14:29:24 +0300

    Add solution - CodeHub

---
+++ 6-kyu/replace-with-alphabet-position/replace-with-alphabet-position.py
@@ -0,0 +1,38 @@
+LETTER_POSITIONS = {
+    "A": 1,
+    "B": 2,
+    "C": 3,
+    "D": 4,
+    "E": 5,
+    "F": 6,
+    "G": 7,
+    "H": 8,
+    "I": 9,
+    "J": 10,
+    "K": 11,
+    "L": 12,
+    "M": 13,
+    "N": 14,
+    "O": 15,
+    "P": 16,
+    "Q": 17,
+    "R": 18,
+    "S": 19,
+    "T": 20,
+    "U": 21,
+    "V": 22,
+    "W": 23,
+    "X": 24,
+    "Y": 25,
+    "Z": 26,
+}
+​
+​
+def alphabet_position(text):
+    result = [
+        str(LETTER_POSITIONS[letter.upper()])
+        for letter in text
+        if letter.upper() in LETTER_POSITIONS
+    ]
+    return " ".join(result)
+​
