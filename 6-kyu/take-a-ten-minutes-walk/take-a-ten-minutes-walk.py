8f5355d33f8c3de3795b4068f5e26e9131bce01f
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-04-09 15:30:59 +0300

    Update solution - CodeHub

--- 6-kyu/take-a-ten-minutes-walk/take-a-ten-minutes-walk.py
+++ 6-kyu/take-a-ten-minutes-walk/take-a-ten-minutes-walk.py
@@ -1,14 +1,8 @@
 def is_valid_walk(walk):
     if len(walk) != 10:
         return False
-    direction_counts = {
-        'n': walk.count('n'),
-        's': walk.count('s'),
-        'w': walk.count('w'),
-        'e': walk.count('e'),
-    }
     return (
-        direction_counts['n'] == direction_counts['s']
-        and direction_counts['w'] == direction_counts['e']
+        walk.count('n') == walk.count('s')
+        and walk.count('w') == walk.count('e')
     )
 ​
