c2686feb3ddfe56b4c2d44ebedb1e4b2ea60bc95
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-04-10 12:37:57 +0300

    Update solution - CodeHub

--- 6-kyu/convert-string-to-camel-case/convert-string-to-camel-case.py
+++ 6-kyu/convert-string-to-camel-case/convert-string-to-camel-case.py
@@ -2,12 +2,6 @@ import re
 ​
 ​
 def to_camel_case(text):
-    text_list = re.split(r'[_\-]', text)
-    result_text = ""
-    for index in range(len(text_list)):
-        if index == 0:
-            result_text += text_list[index]
-        else:
-            result_text += text_list[index].capitalize()
-    return result_text
+    words = re.split(r'[_\-]', text)
+    return words[0] + ''.join(word.capitalize() for word in words[1:])
 ​
