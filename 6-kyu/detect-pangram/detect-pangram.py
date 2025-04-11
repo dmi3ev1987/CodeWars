51fc70198cec146219de08beb3f86f315688b286
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-04-11 12:47:38 +0300

    Update solution - CodeHub

--- 6-kyu/detect-pangram/detect-pangram.py
+++ 6-kyu/detect-pangram/detect-pangram.py
@@ -1,37 +1,8 @@
-ALPHABET_COUNT = {
-    "a": 0,
-    "b": 0,
-    "c": 0,
-    "d": 0,
-    "e": 0,
-    "f": 0,
-    "g": 0,
-    "h": 0,
-    "i": 0,
-    "j": 0,
-    "k": 0,
-    "l": 0,
-    "m": 0,
-    "n": 0,
-    "o": 0,
-    "p": 0,
-    "q": 0,
-    "r": 0,
-    "s": 0,
-    "t": 0,
-    "u": 0,
-    "v": 0,
-    "w": 0,
-    "x": 0,
-    "y": 0,
-    "z": 0,
-}
-​
+from string import ascii_lowercase
 ​
 def is_pangram(st):
-    lower_str = st.lower()
-    alphabet_copy = ALPHABET_COUNT.copy()
-    for letter in lower_str:
-        if letter in alphabet_copy:
-            alphabet_copy[letter] += 1
-    return 0 not in alphabet_copy.values()
+    letters_present = {letter: 0 for letter in ascii_lowercase}
+    for letter in st.lower():
+        if letter in letters_present:
+            letters_present[letter] += 1
+    return all(letters_present.values())
