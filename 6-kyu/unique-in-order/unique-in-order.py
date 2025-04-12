5d91407dcf16e16d5eb4f89ac4345e91c651df8b
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-04-12 12:52:54 +0300

    Update solution - CodeHub

--- 6-kyu/unique-in-order/unique-in-order.py
+++ 6-kyu/unique-in-order/unique-in-order.py
@@ -1,10 +1,7 @@
 def unique_in_order(sequence):
     result_list = []
     for item in sequence:
-        if len(result_list) > 0:
-            if item != result_list[-1]:
-                result_list.append(item)
-        else:
+        if not result_list or item != result_list[-1]:
             result_list.append(item)
     return result_list
 ​
