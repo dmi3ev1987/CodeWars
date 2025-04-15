243f19042076eb023daf22d557087f66c1bbe2f5
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-04-15 12:39:12 +0300

    Update solution - CodeHub

--- 6-kyu/split-strings/split-strings.py
+++ 6-kyu/split-strings/split-strings.py
@@ -1,10 +1,5 @@
 def solution(s):
-    letters_list = list(s)
-    result_list = []
-    for index in range(0, len(s), 2):
-        try:
-            result_list.append(letters_list[index] + letters_list[index + 1])
-        except IndexError:
-            result_list.append(letters_list[index] + "_")
-    return result_list
+    if len(s) % 2 != 0:
+        s += "_"
+    return [s[index:index + 2] for index in range(0, len(s), 2)]
 ​
