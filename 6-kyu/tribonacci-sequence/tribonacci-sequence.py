fd06bb8ba9786934f188fc3c2957eac46b4212fd
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-04-16 12:24:55 +0300

    Add solution - CodeHub

---
+++ 6-kyu/tribonacci-sequence/tribonacci-sequence.py
@@ -0,0 +1,10 @@
+def tribonacci(signature, n):
+    if n < 3:
+        return signature[:n]
+    result = []
+    for index in range(n - 3):
+        signature.append(
+            signature[index] + signature[index + 1] + signature[index + 2]
+        )
+    return signature
+​
