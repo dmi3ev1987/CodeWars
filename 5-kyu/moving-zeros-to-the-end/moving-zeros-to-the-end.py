b0c133a975a609614df64154485a710e1e2ad85c
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-04-19 12:09:57 +0300

    Update solution - CodeHub

--- 5-kyu/moving-zeros-to-the-end/moving-zeros-to-the-end.py
+++ 5-kyu/moving-zeros-to-the-end/moving-zeros-to-the-end.py
@@ -1,4 +1,4 @@
 def move_zeros(lst):
     result_lst = [item for item in lst if item != 0]
-    zero_lst = [0 for item in range(lst.count(0))]
-    return result_lst + zero_lst
+    return result_lst + [0] * (len(lst) - len(result_lst))
+​
