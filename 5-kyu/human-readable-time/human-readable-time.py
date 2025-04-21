83a6769e7753fe9bdb7204895fa8dc359c76bbe7
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-04-21 09:10:51 +0300

    Update solution - CodeHub

--- 5-kyu/human-readable-time/human-readable-time.py
+++ 5-kyu/human-readable-time/human-readable-time.py
@@ -1,11 +1,7 @@
 def make_readable(seconds):
     hours = seconds // 3600
-    minutes = (seconds % 3600) // 60
-    seconds = (seconds % 3600) % 60
-    if hours < 10:
-        hours = "0" + str(hours)
-    if minutes < 10:
-        minutes = "0" + str(minutes)
-    if seconds < 10:
-        seconds = "0" + str(seconds)
-    return f'{hours}:{minutes}:{seconds}'
+    remaining_seconds = seconds % 3600
+    minutes = remaining_seconds // 60
+    seconds = remaining_seconds % 60
+    
+    return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
