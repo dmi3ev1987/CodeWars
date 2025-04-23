ef693dc8462d86f606b591299a8133d540942c06
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-04-23 07:25:48 +0300

    Add solution - CodeHub

---
+++ 5-kyu/the-hashtag-generator/the-hashtag-generator.py
@@ -0,0 +1,9 @@
+def generate_hashtag(s):
+    stripped = s.strip()
+    if not stripped:
+        return False
+    hashtag = "#" + "".join(word.capitalize() for word in stripped.split())
+    if len(hashtag) > 140:
+        return False
+    return hashtag
+​
