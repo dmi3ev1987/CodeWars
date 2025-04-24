88d7c109fe32339fb156a16eb1cca760794dad60
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-04-24 12:35:14 +0300

    Update solution - CodeHub

--- 5-kyu/rot13-1/rot13-1.py
+++ 5-kyu/rot13-1/rot13-1.py
@@ -1,11 +1,18 @@
 def rot13(message):
     result = []
+    shift = 13
     for letter in message:
         if 'a' <= letter <= 'z':
-            result.append(chr(((ord(letter) - ord('a') + 13) % 26) + ord('a')))
+            a = ord('a')
+            result.append(chr(get_shifted_ord_number(a, ord(letter), shift)))
         elif 'A' <= letter <= 'Z':
-            result.append(chr(((ord(letter) - ord('A') + 13) % 26 + ord('A'))))
+            a = ord('A')
+            result.append(chr(get_shifted_ord_number(a, ord(letter), shift)))
         else:
             result.append(letter)
     return ''.join(result)
+​
+​
+def get_shifted_ord_number(ord_start, ord_letter, shift):
+    return ((ord_letter - ord_start + shift) % 26) + ord_start
 ​
