f5f5ea2ffa1469b3d9199d93b4447be8c5de7609
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-04-25 13:42:19 +0300

    Add solution - CodeHub

---
+++ 5-kyu/directions-reduction/directions-reduction.py
@@ -0,0 +1,18 @@
+def dir_reduc(arr):
+    dir_dict = {
+        'NORTH': 'SOUTH',
+        'SOUTH': 'NORTH',
+        'EAST': 'WEST',
+        'WEST': 'EAST',
+    }
+    index = 0
+    while index < len(arr) - 1:
+        if arr[index + 1] == dir_dict[arr[index]]:
+            arr.pop(index)
+            arr.pop(index)
+            if index > 0:
+                index -= 1
+        else:
+            index += 1
+    return arr
+​
