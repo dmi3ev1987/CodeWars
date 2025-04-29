53420047b7bfe0c2468d30ab2d6f13d4693b3478
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-04-29 10:50:58 +0300

    Update solution - CodeHub

--- 5-kyu/first-non-repeating-character/first-non-repeating-character.py
+++ 5-kyu/first-non-repeating-character/first-non-repeating-character.py
@@ -1,8 +1,5 @@
 def first_non_repeating_letter(s):
-    no_reapet_letter = ''
     for letter in s:
         if s.lower().count(letter.lower()) == 1:
-            no_reapet_letter = letter
-            break
-    return no_reapet_letter
-​
+            return letter
+    return ''
