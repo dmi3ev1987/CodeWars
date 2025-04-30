4d7c999a246632a5f41a32b1fe3b9c02a13e88f6
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-04-30 10:57:08 +0300

    Update solution - CodeHub

--- 5-kyu/product-of-consecutive-fib-numbers/product-of-consecutive-fib-numbers.py
+++ 5-kyu/product-of-consecutive-fib-numbers/product-of-consecutive-fib-numbers.py
@@ -1,18 +1,5 @@
 def product_fib(_prod):
-    fibo_numbers = [0, 1]
-    count = 1
-    result = 0
-    while result <= _prod:
-        f = fibo_numbers[count]
-        f_previous = fibo_numbers[count - 1]
-        fibo_numbers.append(f + f_previous)
-        count += 1  
-        result = f * f_previous 
-    if result > _prod:
-        count -= 2
-        f = fibo_numbers[count]
-        f_previous = fibo_numbers[count - 1]
-        previous_result = f * f_previous
-        if previous_result == _prod:
-            return [f_previous, f, True]
-    return [f, fibo_numbers[count+1], False]
+    a, b = 0, 1
+    while a * b < _prod:
+        a, b = b, a + b
+    return [a, b, a * b == _prod]
