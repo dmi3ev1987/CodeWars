7d6963a244ca103f77ac7fc6b6298728e67d34b9
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-05-02 13:34:55 +0300

    Add solution - CodeHub

---
+++ 5-kyu/scramblies/scramblies.py
@@ -0,0 +1,8 @@
+from collections import Counter
+​
+def scramble(s1, s2):
+    s1_counts = Counter(s1)
+    for letter, count in Counter(s2).items():
+        if s1_counts[letter] < count:
+            return False
+    return True
