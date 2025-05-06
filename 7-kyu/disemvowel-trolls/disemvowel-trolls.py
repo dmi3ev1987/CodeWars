59201e6ab476294c4393509d122cb4eb0133922d
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-05-06 14:23:45 +0300

    Update solution - CodeHub

--- 7-kyu/disemvowel-trolls/disemvowel-trolls.py
+++ 7-kyu/disemvowel-trolls/disemvowel-trolls.py
@@ -1,10 +1,7 @@
 def disemvowel(string_):
-    vowels = 'aeiou'
-    word_list = string_.split(" ")
-    new_list = []
-    for word in word_list:
-        new_word = [
-            letter for letter in word if letter.lower() not in vowels 
-        ]
-        new_list.append(''.join(new_word))
-    return ' '.join(new_list)
+    vowels = {'a', 'e', 'i', 'o', 'u'}
+    return ' '.join(
+        ''.join(letter for letter in word if letter.lower() not in vowels)
+        for word in string_.split(' ')
+    )
+​
