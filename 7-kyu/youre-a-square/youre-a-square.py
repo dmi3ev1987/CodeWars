3025f9d8a4510632a614cae3d1a4848b999ba435
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-05-08 22:41:50 +0300

    Update solution - CodeHub

--- 7-kyu/youre-a-square/youre-a-square.py
+++ 7-kyu/youre-a-square/youre-a-square.py
@@ -7,8 +7,8 @@ def is_square(n):
     while count**2 <= n:
         if count**2 == n:
             return True
-        elif count**2 > n:
+        if count**2 > n:
             return False
-        else:
-            count += 1
+        count += 1
     return False
+​
