381a17c525f3e0c9b0803a88e62ada4dcd2a92ea
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-05-08 20:13:09 +0000

    Fix your order.

--- 6-kyu/your-order-please/your-order-please.py
+++ 6-kyu/your-order-please/your-order-please.py
@@ -1,8 +1,8 @@
 def order(sentence):
     sentence_list = sentence.split()
     result_list = []
-    for index in range(1, len(sentence_list)+1):
+    for index in range(1, len(sentence_list) + 1):
         for word in sentence_list:
             if str(index) in word:
-                result_list.append(word)
-    return " ".join(result_list)
+                result_list.append(word)  # noqa: PERF401
+    return ' '.join(result_list)
