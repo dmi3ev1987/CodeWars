ceb414428b653433334b6f7dacfa93fa229f492b
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-05-08 21:49:49 +0300

    Update solution - CodeHub

--- 4-kyu/human-readable-duration-format/human-readable-duration-format.py
+++ 4-kyu/human-readable-duration-format/human-readable-duration-format.py
@@ -1,23 +1,3 @@
-def format_duration(seconds) -> str:
-    if seconds == 0:
-        return 'now'
-​
-    duration = get_duration_dict(seconds)
-​
-    result = [
-        f'{duration[item]} {item[:-1] if duration[item] == 1 else item}'
-        for item in duration
-        if duration[item] != 0
-    ]
-​
-    return (
-        ', '.join(result[:-1]) + ' and ' + result[-1]
-        if len(result) > 1
-        else result[0]
-    )
-​
-​
-def get_duration_dict(seconds) -> dict:
     coefficient = 60 * 60 * 24 * 365
     years = seconds // coefficient
 ​
@@ -31,3 +11,15 @@ def get_duration_dict(seconds) -> dict:
 ​
     seconds = seconds % coefficient
     coefficient = 60
+    minutes = seconds // coefficient
+​
+    seconds = seconds % coefficient
+​
+    return {
+        'years': years,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds,
+    }
+​
