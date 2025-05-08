e94409140b4e0acbcef90340453e9aa95a253597
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-05-08 20:20:31 +0000

    Fix multiplies 3 or 5.

--- 6-kyu/multiples-of-3-or-5/multiples-of-3-or-5.py
+++ 6-kyu/multiples-of-3-or-5/multiples-of-3-or-5.py
@@ -4,15 +4,14 @@ def solution(number):
         item for item in natural_list if item % 3 == 0 or item % 5 == 0
     ]
     return get_sum_of_numbers(result_list)
-​
-​
+
+
 def get_natural_list(number):
     return [item for item in range(number) if item != 0]
-​
-​
+
+
 def get_sum_of_numbers(result_list):
     result = 0
     for item in result_list:
         result += item
     return result
-​
