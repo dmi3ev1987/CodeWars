af9db3c73b316887e3e01cae9c1a76905ed58f30
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-05-08 22:54:27 +0300

    Update solution - CodeHub

--- 7-kyu/sum-of-the-first-nth-term-of-series/sum-of-the-first-nth-term-of-series.py
+++ 7-kyu/sum-of-the-first-nth-term-of-series/sum-of-the-first-nth-term-of-series.py
@@ -2,16 +2,17 @@ def series_sum(n):
     result = 0
     temp_list = [1]
     if n > 1:
-        for item in range(1, n):
-            temp_list.append(temp_list[-1]+3)
+        for _ in range(1, n):
+            temp_list.append(temp_list[-1] + 3)
         result = calc(temp_list)
     else:
         result = n
-    return r"{:.2f}".format(result)
-
-
+    return f'{result:.2f}'
+​
+​
 def calc(list):
     result = 0
     for item in list:
-        result += 1/item
+        result += 1 / item
     return round(result, 2)
+​
