aca200d966273bcc7f85fd1fb271f7ef63b0752a
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-05-08 20:34:27 +0000

    Fix find odd int.

--- 6-kyu/find-the-odd-int/find-the-odd-int.py
+++ 6-kyu/find-the-odd-int/find-the-odd-int.py
@@ -5,18 +5,17 @@ def find_it(seq):
         result_dict[number] = count
         seq = new_seq
     return find_one_odd(result_dict)
-​
-​
+
+
 def count_number(seq):
     number = seq[0]
     count = seq.count(number)
     result_seq = [item for item in seq if item != number]
     return number, count, result_seq
-​
-​
+
+
 def find_one_odd(input_dict):
     return next(
         (key for key, value in input_dict.items() if value % 2 != 0),
         None,
     )
-​
