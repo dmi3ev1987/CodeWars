04ccc43e22c19147bc028431c325e1b0bf276879
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-05-09 11:12:57 +0300

    Update solution - CodeHub

--- 7-kyu/highest-and-lowest/highest-and-lowest.py
+++ 7-kyu/highest-and-lowest/highest-and-lowest.py
@@ -1,7 +1,3 @@
 def high_and_low(numbers):
-    numbers_list = numbers.split(' ')
-    min_number = max_number = int(numbers_list[0])
-    for number in numbers_list:
-        min_number = min(min_number, int(number))
-        max_number = max(max_number, int(number))
-    return f'{max_number} {min_number}'
+    numbers = [int(number) for number in numbers.split(' ')]
+    return f'{max(numbers)} {min(numbers)}'
