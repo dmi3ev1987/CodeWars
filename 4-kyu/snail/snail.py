777d2e93917fa43b30a0ea175cce899ee08a74f6
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2025-05-10 13:06:09 +0000

    Fix snail pop.

--- 4-kyu/snail/snail.py
+++ 4-kyu/snail/snail.py
@@ -2,14 +2,14 @@ def snail(snail_map):
     result = []
     while snail_map:
         result += snail_map.pop(0)
-        
+
         if snail_map and snail_map[0]:
             for row in snail_map:
                 result.append(row.pop())
-        
+
         if snail_map:
             result += snail_map.pop()[::-1]
-        
+
         if snail_map and snail_map[0]:
             for row in snail_map[::-1]:
                 result.append(row.pop(0))
