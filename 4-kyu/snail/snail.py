79a745e329d352cb3c585c1cafbd3b2a44542249
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-05-10 15:47:25 +0300

    Update solution - CodeHub

--- 4-kyu/snail/snail.py
+++ 4-kyu/snail/snail.py
@@ -1,18 +1,16 @@
 def snail(snail_map):
     result = []
     start = 0
-    stop = len(snail_map)
-​
+    stop = len(snail_map) # 5
     while start < stop:
         result.extend(snail_map[start][start:stop])
 ​
         for index in range(start + 1, stop):
             result.append(snail_map[index][stop - 1])
-​
+        
         if start != stop - 1:
-            result.extend(snail_map[stop - 1][start : stop - 1][::-1])
+            result.extend(snail_map[stop - 1][start:stop - 1][::-1])
 ​
-        if start != stop - 1:
             for index in range(stop - 2, start, -1):
                 result.append(snail_map[index][start])
 ​
