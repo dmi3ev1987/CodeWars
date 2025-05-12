63f6d8492e671b4380cfde1ed508d9e89dcbd7c5
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-05-12 10:49:49 +0300

    Update solution - CodeHub

--- 7-kyu/descending-order/descending-order.py
+++ 7-kyu/descending-order/descending-order.py
@@ -1,5 +1,4 @@
 def descending_order(numbers):
-    numbers_list = [int(number) for number in str(numbers)]
-    numbers_list.sort(reverse=True)
-    str_list = [str(number) for number in numbers_list]
-    return int(''.join(str_list))
+    numbers_list = list(str(numbers))
+    return int(''.join(reversed(sorted(numbers_list))))
+​
