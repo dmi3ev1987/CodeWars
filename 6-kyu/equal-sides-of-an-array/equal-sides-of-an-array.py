9b872d9da39f079f3ca0ed72955a1c511c41c719
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-05-17 13:30:35 +0300

    Add solution - CodeHub

---
+++ 6-kyu/equal-sides-of-an-array/equal-sides-of-an-array.py
@@ -0,0 +1,9 @@
+def find_even_index(array):
+    left = 0
+    for index in range(len(array)):
+        if index != 0:
+            left = sum(array[:index])
+        right = sum(array[index+1:])
+        if left == right:
+            return index
+    return -1
