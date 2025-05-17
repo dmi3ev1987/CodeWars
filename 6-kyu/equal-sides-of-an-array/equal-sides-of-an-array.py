46684d3d01021875ed1b99aee62b84d18bf6fe51
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2025-05-17 10:32:21 +0000

    Equal Sides Of An Array.

--- 6-kyu/equal-sides-of-an-array/equal-sides-of-an-array.py
+++ 6-kyu/equal-sides-of-an-array/equal-sides-of-an-array.py
@@ -3,7 +3,7 @@ def find_even_index(array):
     for index in range(len(array)):
         if index != 0:
             left = sum(array[:index])
-        right = sum(array[index+1:])
+        right = sum(array[index + 1 :])
         if left == right:
             return index
     return -1
