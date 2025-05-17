e88a5fb4d7a418a71f9d576798da6001b22db15a
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-05-17 23:17:34 +0300

    Add solution - CodeHub

---
+++ 4-kyu/range-extraction/range-extraction.py
@@ -0,0 +1,22 @@
+def solution(array):
+    result = []
+    while array:
+        number_to_append = array.pop(0)
+        if not array:
+            result.append(str(number_to_append))
+            break
+        if number_to_append + 1 != array[0]:
+            result.append(str(number_to_append))
+        else:
+            next_number = array[0] + 1
+            for index in range(len(array)):
+                if index != len(array) - 1 and array[index + 1] == next_number:
+                    next_number = array[index + 1] + 1
+                else:
+                    if index >= 1:
+                        result.append(f'{number_to_append}-{array[index]}')
+                        array = array[index+1:]
+                        break
+                    result.append(str(number_to_append))
+                    break
+    return ','.join(result)
