604c7cec6c96cfe716e05db8fccf7782f2251776
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-05-17 11:52:07 +0300

    Update solution - CodeHub

--- 7-kyu/exes-and-ohs/exes-and-ohs.py
+++ 7-kyu/exes-and-ohs/exes-and-ohs.py
@@ -1,9 +1,2 @@
 def xo(string):
-    counter = {
-        'x': 0,
-        'o': 0,
-    }
-    for letter in string.lower():
-        if letter in counter:
-            counter[letter] += 1
-    return counter['x'] == counter['o']
+    return string.lower().count('x') == string.lower().count('o')
