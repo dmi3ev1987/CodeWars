2beefd0705f989170c71e05e28e6dd98d2408a7e
Andrey Dmitriev <andrey.dmi3ev@gmail.com>
2025-05-18 18:38:37 +0300

    Change append to extend.

--- 4-kyu/snail/snail_start_stop.py
+++ 4-kyu/snail/snail_start_stop.py
@@ -6,13 +6,13 @@ def snail(snail_map):
         result.extend(snail_map[start][start:stop])
 
         for index in range(start + 1, stop):
-            result.append(snail_map[index][stop - 1])
+            result.extend(snail_map[index][stop - 1])
 
         if start != stop - 1:
             result.extend(snail_map[stop - 1][start : stop - 1][::-1])
 
             for index in range(stop - 2, start, -1):
-                result.append(snail_map[index][start])
+                result.extend(snail_map[index][start])
 
         start += 1
         stop -= 1
