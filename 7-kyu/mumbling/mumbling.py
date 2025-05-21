b0cccdbb519722de7a21e80fa79ed888aed314ae
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-05-21 11:28:44 +0300

    Update solution - CodeHub

--- 7-kyu/mumbling/mumbling.py
+++ 7-kyu/mumbling/mumbling.py
@@ -1,5 +1,5 @@
 def accum(inner_string):
     return '-'.join(
-        (inner_string[index] * (index + 1)).capitalize()
-        for index in range(len(inner_string))
+        letter.upper() + letter.lower() * index
+        for index, letter in enumerate(inner_string)
     )
