b2d229146c864aa5fa49946bf1968f3229b7ab25
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-05-24 16:31:48 +0300

    Update solution - CodeHub

--- 4-kyu/strip-comments/strip-comments.py
+++ 4-kyu/strip-comments/strip-comments.py
@@ -1,8 +1,8 @@
 def strip_comments(input_string, markers_list):
     string_lines = input_string.split('\n')
-    for index in range(len(string_lines)):
-        for marker in markers_list:
-            string_lines[index] = (
-                str(string_lines[index]).split(marker)[0].rstrip()
-            )
+    for marker in markers_list:
+        string_lines = [
+            line.split(marker)[0].rstrip()
+            for line in string_lines
+        ]
     return '\n'.join(string_lines)
