6397a173fe5dbb447a881e6100906f20e405cd82
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-05-24 15:02:17 +0300

    Update solution - CodeHub

--- 7-kyu/beginner-series-number-3-sum-of-numbers/beginner-series-number-3-sum-of-numbers.py
+++ 7-kyu/beginner-series-number-3-sum-of-numbers/beginner-series-number-3-sum-of-numbers.py
@@ -1,9 +1,2 @@
 def get_sum(a, b):
-    if a == b:
-        return a
-    if a > b:
-        a, b = b, a
-    number_sum = 0
-    for number in range(a, b + 1):
-        number_sum += number
-    return number_sum
+    return sum(range(min(a, b), max(a, b) + 1))
