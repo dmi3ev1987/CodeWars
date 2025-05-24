1ad4505826b174f20be63f5aeee61b9750f9d7b0
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2025-05-24 13:34:41 +0000

    Strip Comments.

--- 4-kyu/strip-comments/strip-comments.py
+++ 4-kyu/strip-comments/strip-comments.py
@@ -2,7 +2,6 @@ def strip_comments(input_string, markers_list):
     string_lines = input_string.split('\n')
     for marker in markers_list:
         string_lines = [
-            line.split(marker)[0].rstrip()
-            for line in string_lines
+            line.split(marker)[0].rstrip() for line in string_lines
         ]
     return '\n'.join(string_lines)
