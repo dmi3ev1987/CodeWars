a0bbe80462e49217e99f6acbdb004ae861f67a53
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-05-26 11:31:20 +0300

    Update solution - CodeHub

--- 7-kyu/credit-card-mask/credit-card-mask.py
+++ 7-kyu/credit-card-mask/credit-card-mask.py
@@ -1,4 +1,2 @@
 def maskify(card_number):
-    if len(str(card_number)) > 4:
-        return '#' * (len(str(card_number)) - 4) + str(card_number)[-4:]
-    return card_number
+    return '#' * (len(str(card_number)) - 4) + str(card_number)[-4:]
