f1ab0b6b21ffd59667583d0ebfe3d57f66479b53
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-06-02 23:38:46 +0300

    Add solution - CodeHub

---
+++ 7-kyu/sum-of-odd-numbers/sum-of-odd-numbers.py
@@ -0,0 +1,11 @@
+def row_sum_odd_numbers(n):
+    current_odd = 1
+    result = []
+    for index in range(1, n + 1):
+        item = []
+        for odd_number in range(index):
+            item.append(current_odd)
+            current_odd += 2
+        result.append(item)
+​
+    return sum(result[-1])
