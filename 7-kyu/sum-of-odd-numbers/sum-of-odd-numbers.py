8011609d9875751af9ed06b563a8d5dcc49b3a4e
Andrey Dmitriev <andrey.dmi3ev@gmail.com>
2025-06-02 23:49:57 +0300

    Sum of odd numbers.

--- 7-kyu/sum-of-odd-numbers/sum-of-odd-numbers.py
+++ 7-kyu/sum-of-odd-numbers/sum-of-odd-numbers.py
@@ -4,4 +4,8 @@ def row_sum_odd_numbers(n):
     # The last number in the nth row is n² + n - 1
     last_number = n * n + n - 1
     # Sum of consecutive odd numbers from a to b is ((b - a) // 2 + 1) * (a + b) // 2
-    return ((last_number - first_number) // 2 + 1) * (first_number + last_number) // 2
+    return (
+        ((last_number - first_number) // 2 + 1)
+        * (first_number + last_number)
+        // 2
+    )
