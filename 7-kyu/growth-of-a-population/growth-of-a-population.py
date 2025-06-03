0db36d60bce90d50a3e5e84c291f60ca57b299e6
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-06-03 17:30:11 +0300

    Add solution - CodeHub

---
+++ 7-kyu/growth-of-a-population/growth-of-a-population.py
@@ -0,0 +1,6 @@
+def nb_year(start_population, percent, addition_population, target_population):
+    year_count = 0
+    while start_population < target_population:
+        start_population += int(start_population * percent / 100 + addition_population)
+        year_count += 1
+    return year_count
