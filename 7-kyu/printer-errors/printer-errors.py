c6696c14db38e6474d57416725c6326a6a1ce0ba
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-06-04 23:12:06 +0300

    Update solution - CodeHub

--- 7-kyu/printer-errors/printer-errors.py
+++ 7-kyu/printer-errors/printer-errors.py
@@ -1,6 +1,2 @@
 def printer_error(input_string):
-    count = 0
-    for letter in input_string:
-        if 'm' < letter <= 'z':
-            count += 1
-    return f'{count}/{len(input_string)}'
+    return f'{sum(letter > "m" for letter in input_string)}/{len(input_string)}'
