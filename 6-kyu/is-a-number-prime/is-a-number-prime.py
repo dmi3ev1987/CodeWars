340dac85d1fc2144a07e0d29e2865d89874f368a
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-06-07 15:59:25 +0300

    Add solution - CodeHub

---
+++ 6-kyu/is-a-number-prime/is-a-number-prime.py
@@ -0,0 +1,8 @@
+def is_prime(number):
+    if number < 2:
+        return False
+    for current_number in range(2, int(number ** 0.5) + 1):
+        if number % current_number == 0:
+            return False
+    return True
+​
