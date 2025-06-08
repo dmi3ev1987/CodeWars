dcd13c41fcb7a8f168c3cb96ddefef44313d8ee7
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-06-08 22:19:36 +0300

    Update solution - CodeHub

--- 5-kyu/perimeter-of-squares-in-a-rectangle/perimeter-of-squares-in-a-rectangle.py
+++ 5-kyu/perimeter-of-squares-in-a-rectangle/perimeter-of-squares-in-a-rectangle.py
@@ -1,10 +1,7 @@
 def perimeter(n):
-    if n <= 0:
-        return 4
-
-    a, b = 1, 1
+    a, b = 0, 1
     total = a + b
-    for _ in range(2, n + 1):
+    for _ in range(n):
         a, b = b, a + b
         total += b
     return total * 4
