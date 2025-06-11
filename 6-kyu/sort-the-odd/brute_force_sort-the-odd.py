9848bb7a8842c172a5e7d9ccc93da29762600dfb
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2025-06-11 14:43:52 +0000

    Sort the odd.

---
+++ 6-kyu/sort-the-odd/brute_force_sort-the-odd.py
@@ -0,0 +1,9 @@
+def sort_array(source_array):
+    odds = sorted(number for number in source_array if number % 2)
+    odd_index = 0
+
+    for index in range(len(source_array)):
+        if source_array[index] % 2:
+            source_array[index] = odds[odd_index]
+            odd_index += 1
+    return source_array
