8949f6664108092849d79e2a7555a07b8d9fca5c
Andrey Dmitriev <andrey.dmi3ev@gmail.com>
2025-06-11 08:28:50 +0300

    Sort the odd.

--- 6-kyu/sort-the-odd/sort-the-odd.py
+++ 6-kyu/sort-the-odd/sort-the-odd.py
@@ -1,7 +1,7 @@
 def sort_array(source_array):
     odds = sorted(number for number in source_array if number % 2)
     odd_index = 0
-​
+
     for index in range(len(source_array)):
         if source_array[index] % 2:
             source_array[index] = odds[odd_index]
