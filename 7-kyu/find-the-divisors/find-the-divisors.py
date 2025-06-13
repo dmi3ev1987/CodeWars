1d61f0d3482147e76cb1e984fd8c1301a7680081
Andrey Dmitriev <andrey.dmi3ev@gmail.com>
2025-06-13 19:55:02 +0300

    Brute force. Find the divisors!

--- 7-kyu/find-the-divisors/find-the-divisors.py
+++ 7-kyu/find-the-divisors/find-the-divisors.py
@@ -1,7 +1,5 @@
 def divisors(integer):
-    result = [
-        number for number in range(2, integer) if integer % number == 0
-    ]
+    result = [number for number in range(2, integer) if integer % number == 0]
     if result:
         return result
     return f'{integer} is prime'
