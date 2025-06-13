534f84b438f27f6e67dd83dc9f376f70a980d40d
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-06-13 19:57:35 +0300

    Update solution - CodeHub

--- 7-kyu/find-the-divisors/find-the-divisors.py
+++ 7-kyu/find-the-divisors/find-the-divisors.py
@@ -1,5 +1,3 @@
 def divisors(integer):
     result = [number for number in range(2, integer) if integer % number == 0]
-    if result:
-        return result
-    return f'{integer} is prime'
+    return result if result else f'{integer} is prime'
