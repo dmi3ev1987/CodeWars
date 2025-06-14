26de8957ca6a879e1a63ff0b18f231f80d28acd2
Andrey Dmitriev <andrey.dmi3ev@gmail.com>
2025-06-14 19:49:15 +0300

    Remove the minimum.

--- 7-kyu/remove-the-minimum/remove-the-minimum.py
+++ 7-kyu/remove-the-minimum/remove-the-minimum.py
@@ -2,4 +2,4 @@ def remove_smallest(input_numbers):
     if len(input_numbers) < 1:
         return input_numbers
     index = input_numbers.index(min(input_numbers))
-    return input_numbers[:index] + input_numbers[index+1:]
+    return input_numbers[:index] + input_numbers[index + 1 :]
