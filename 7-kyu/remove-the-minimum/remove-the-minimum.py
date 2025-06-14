a23d1d2e52809beed952a2ddfe160d6b4f65d5fb
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-06-14 19:44:26 +0300

    Update solution - CodeHub

--- 7-kyu/remove-the-minimum/remove-the-minimum.py
+++ 7-kyu/remove-the-minimum/remove-the-minimum.py
@@ -1,9 +1,5 @@
 def remove_smallest(input_numbers):
-    if len(input_numbers) == 0 or len(input_numbers) == 1:
-        return []
-    min_number = min(input_numbers)
-    for index in range(len(input_numbers)):
-        if input_numbers[index] == min_number:
-            input_numbers = input_numbers[:index] + input_numbers[index + 1 :]
-            break
-    return input_numbers
+    result = input_numbers[:]
+    if result:
+        result.remove(min(result))
+    return result
