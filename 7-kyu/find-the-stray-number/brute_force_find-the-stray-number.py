a00f05d1400b2221928cb609428b25fa482fbfde
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2025-06-17 14:18:55 +0000

    Brute force. Find the stray number.

---
+++ 7-kyu/find-the-stray-number/brute_force_find-the-stray-number.py
@@ -0,0 +1,12 @@
+def stray(input_array):
+    unique = (
+        input_array[0] if input_array[0] != input_array[1] else input_array[-1]
+    )
+    for index in range(1, len(input_array) - 1):
+        if (
+            input_array[index - 1] != input_array[index]
+            and input_array[index + 1] != input_array[index]
+        ):
+            unique = input_array[index]
+            break
+    return unique
