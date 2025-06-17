0c9d87ba063152039dbfe156e317af1ae89a5fd1
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-06-17 17:34:02 +0300

    Update solution - CodeHub

--- 7-kyu/find-the-stray-number/find-the-stray-number.py
+++ 7-kyu/find-the-stray-number/find-the-stray-number.py
@@ -1,12 +1,7 @@
 def stray(input_array):
-    unique = (
-        input_array[0] if input_array[0] != input_array[1] else input_array[-1]
-    )
-    for index in range(1, len(input_array) - 1):
-        if (
-            input_array[index - 1] != input_array[index]
-            and input_array[index + 1] != input_array[index]
-        ):
-            unique = input_array[index]
-            break
-    return unique
+    if input_array.count(input_array[0]) == 1:
+        return input_array[0]
+    for number in input_array:
+        if number != input_array[0]:
+            return number
+    return None
