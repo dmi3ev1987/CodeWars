9b9c683b84a84384447f74f7117f8c73401bd2e2
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-06-19 18:30:04 +0300

    Update solution - CodeHub

--- 7-kyu/make-a-function-that-does-arithmetic/make-a-function-that-does-arithmetic.py
+++ 7-kyu/make-a-function-that-does-arithmetic/make-a-function-that-does-arithmetic.py
@@ -1,10 +1,10 @@
 def arithmetic(a, b, operator):
-    if operator == 'add':
-        return a + b
-    if operator == 'subtract':
-        return a - b
-    if operator == 'multiply':
-        return a * b
-    if operator == 'divide':
-        return a / b
+    operators = {
+        'add': lambda: a + b,
+        'subtract': lambda: a - b,
+        'multiply': lambda: a * b,
+        'divide': lambda: a / b,
+    }
+    if operator in operators:
+        return operators[operator]()
     return None
