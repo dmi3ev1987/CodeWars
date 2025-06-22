e34b0c517819e193251022e9670c8578caa4e589
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2025-06-22 14:58:11 +0000

    PaginationHelper.

--- 5-kyu/paginationhelper/paginationhelper.py
+++ 5-kyu/paginationhelper/paginationhelper.py
@@ -6,13 +6,13 @@ class PaginationHelper:
         self._page_count = (
             self._item_count + items_per_page - 1
         ) // items_per_page
-​
+
     def item_count(self):
         return self._item_count
-​
+
     def page_count(self):
         return self._page_count
-​
+
     def page_item_count(self, page_index):
         if page_index < 0 or page_index >= self._page_count:
             return -1
@@ -21,7 +21,7 @@ class PaginationHelper:
             if page_index == self._page_count - 1
             else self.items_per_page
         )
-​
+
     def page_index(self, item_index):
         if item_index < 0 or item_index >= self._item_count:
             return -1
