74b4d97e893c7c9e9c6ecba0ec0d776dc397236e
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2025-06-22 10:37:43 +0000

    Counter. Are they the same?

---
+++ 6-kyu/are-they-the-same/counter_are-they-the-same.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+
+def comp(array, square_array):
+    if array is None or square_array is None:
+        return False
+    if not array and not square_array:
+        return True
+    return Counter(number * number for number in array) == Counter(square_array)
