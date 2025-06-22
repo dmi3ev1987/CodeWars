8997c1fa21b2c892cc0d463d45541da4355fb9ae
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-06-22 13:35:35 +0300

    Update solution - CodeHub

--- 6-kyu/are-they-the-same/are-they-the-same.py
+++ 6-kyu/are-they-the-same/are-they-the-same.py
@@ -1,11 +1,6 @@
 def comp(array, square_array):
-    if array == [] and square_array == []:
-        return True
-    if not array or not square_array:
+    if array is None or square_array is None:
         return False
-    for number in array:
-        if number * number not in square_array:
-            return False
-        else:
-            square_array.pop(square_array.index(number * number))
-    return True
+    if not array and not square_array:
+        return True
+    return sorted(number * number for number in array) == sorted(square_array)
