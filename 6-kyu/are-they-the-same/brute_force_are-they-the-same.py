a84f8c8ad7aec8a0d2669d5cfc2a971260373497
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2025-06-22 10:28:45 +0000

    Brute force. Are they the same?

---
+++ 6-kyu/are-they-the-same/brute_force_are-they-the-same.py
@@ -0,0 +1,11 @@
+def comp(array, square_array):
+    if array == [] and square_array == []:
+        return True
+    if not array or not square_array:
+        return False
+    for number in array:
+        if number * number not in square_array:
+            return False
+        else:
+            square_array.pop(square_array.index(number * number))
+    return True
