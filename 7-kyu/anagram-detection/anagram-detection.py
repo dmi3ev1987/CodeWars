fc9f901d2807061033dcf22ae80c55e2e8dd20b2
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-06-22 13:04:40 +0300

    Update solution - CodeHub

--- 7-kyu/anagram-detection/anagram-detection.py
+++ 7-kyu/anagram-detection/anagram-detection.py
@@ -1,7 +1,2 @@
-from collections import Counter
-
-
 def is_anagram(test, original):
-    if len(test) != len(original):
-        return False
-    return Counter(test.lower()) == Counter(original.lower())
+    return sorted(test.lower()) == sorted(original.lower())
