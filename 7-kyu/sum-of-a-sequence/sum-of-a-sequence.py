4556257540358e98b182c954c122cc9b38c9774d
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-06-23 10:37:20 +0300

    Add solution - CodeHub

---
+++ 7-kyu/sum-of-a-sequence/sum-of-a-sequence.py
@@ -0,0 +1,7 @@
+def sequence_sum(begin_number, end_number, step):
+    result = 0
+    while begin_number <= end_number:
+        if begin_number <= end_number:
+            result += begin_number
+        begin_number += step
+    return result
