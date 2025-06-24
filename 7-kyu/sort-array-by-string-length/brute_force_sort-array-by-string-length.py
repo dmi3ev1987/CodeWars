2d6b8a389e6545c3fb7518d0cb230a9b6141dadf
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2025-06-24 14:07:35 +0000

    Brute force. Sort array by string length.

--- 7-kyu/sort-array-by-string-length/brute_force_sort-array-by-string-length.py
+++ 7-kyu/sort-array-by-string-length/brute_force_sort-array-by-string-length.py
@@ -1,5 +1,3 @@
 def sort_by_length(input_array):
-    result_dict = {}
-    for word in input_array:
-        result_dict[len(word)] = word
-    return [result_dict[key] for key in sorted(result_dict.keys())]
+    words_dict = {len(word): word for word in input_array}
+    return [words_dict[key] for key in sorted(words_dict.keys())]
