03e2107bdb62f66f94b2e3d4a3ad1ef881167e41
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2025-06-25 14:48:11 +0000

    Count the Digit.

--- 7-kyu/count-the-digit/count-the-digit.py
+++ 7-kyu/count-the-digit/count-the-digit.py
@@ -1,2 +1,4 @@
 def nb_dig(number, digit):
-    return sum(str(number * number).count(str(digit)) for number in range(number + 1))
+    return sum(
+        str(number * number).count(str(digit)) for number in range(number + 1)
+    )
