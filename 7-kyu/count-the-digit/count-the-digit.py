a1fd3d7fa7ab1e8efe5632eb64e178f2b19505ed
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-06-25 17:47:25 +0300

    Update solution - CodeHub

--- 7-kyu/count-the-digit/count-the-digit.py
+++ 7-kyu/count-the-digit/count-the-digit.py
@@ -1,5 +1,2 @@
 def nb_dig(number, digit):
-    count = 0
-    for number in range(number + 1):
-        count += str(number * number).count(str(digit))
-    return count
+    return sum(str(number * number).count(str(digit)) for number in range(number + 1))
