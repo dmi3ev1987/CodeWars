7c2f70745ab8d6381b77896dd444feabad2d6ec2
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-06-28 15:55:16 +0300

    Update solution - CodeHub

--- 7-kyu/money-money-money/money-money-money.py
+++ 7-kyu/money-money-money/money-money-money.py
@@ -1,7 +1,6 @@
 def calculate_years(principal, interest, tax, desired):
-    count = 0
+    years_count = 0
     while principal < desired:
-        income = principal * interest
-        principal += income - income * tax
-        count += 1
-    return count
+        principal += principal * interest * (1 - tax)
+        years_count += 1
+    return years_count
