89b97718157c2c9eca3850fbb04e204ae1469864
Andrey Dmitriev <andrey.dmi3ev@gmail.com>
2025-06-28 15:54:17 +0300

    Money, Money, Money.

--- 7-kyu/money-money-money/money-money-money.py
+++ 7-kyu/money-money-money/money-money-money.py
@@ -2,6 +2,6 @@ def calculate_years(principal, interest, tax, desired):
     count = 0
     while principal < desired:
         income = principal * interest
-        principal +=  income - income * tax
+        principal += income - income * tax
         count += 1
     return count
