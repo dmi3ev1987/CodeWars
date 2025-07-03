111ed2ebd08e363e4447e51077dc1dd6b95944a8
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2025-07-03 06:59:47 +0000

    Brute force. Don't give me five!

--- 7-kyu/dont-give-me-five/dont-give-me-five.py
+++ 7-kyu/dont-give-me-five/dont-give-me-five.py
@@ -1,2 +1,4 @@
 def dont_give_me_five(start, end):
-    return len([number for number in range(start, end + 1) if '5' not in str(number)])
+    return len(
+        [number for number in range(start, end + 1) if '5' not in str(number)]
+    )
