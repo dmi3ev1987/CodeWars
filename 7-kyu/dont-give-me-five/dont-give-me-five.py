7bc023187d8cc6f87e72d7675a55766fcadae70d
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-07-03 10:01:34 +0300

    Update solution - CodeHub

--- 7-kyu/dont-give-me-five/dont-give-me-five.py
+++ 7-kyu/dont-give-me-five/dont-give-me-five.py
@@ -1,4 +1,2 @@
 def dont_give_me_five(start, end):
-    return len(
-        [number for number in range(start, end + 1) if '5' not in str(number)]
-    )
+    return sum('5' not in str(number) for number in range(start, end + 1))
