db9eea038e4fa677a7b8188109b7f7380e0bb8fa
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-07-09 14:30:46 +0300

    Update solution - CodeHub

--- 6-kyu/count-the-smiley-faces/count-the-smiley-faces.py
+++ 6-kyu/count-the-smiley-faces/count-the-smiley-faces.py
@@ -1,12 +1,7 @@
+import re
+​
+​
 def count_smileys(input_array):
-    allowed_smile = {':', ';', '-', '~', ')', 'D'}
-    eyes = {':', ';'}
-    smiles = {')', 'D'}
-    noses = {'-', '~'}
     return sum(
-        sum(part in eyes for part in smile) == 1
-        and sum(part in smiles for part in smile) == 1
-        and sum(part in noses for part in smile) <= 1
-        and all(part in allowed_smile for part in smile)
-        for smile in input_array
+        1 for smile in input_array if re.match(r'^[:;][-~]?[)D]$', smile)
     )
