23b58509b9269a02379250fb0d2279765ed1efc0
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-07-12 11:11:25 +0300

    Update solution - CodeHub

--- 6-kyu/bouncing-balls/bouncing-balls.py
+++ 6-kyu/bouncing-balls/bouncing-balls.py
@@ -1,8 +1,10 @@
 def bouncing_ball(height, bounce, window):
-    if height < 0 or bounce < 0 or bounce >= 1 or window >= height:
-        return -1
+    if not 0 < bounce < 1:
+        return - 1
     count = 0
     while height > window:
         height *= bounce
-        count += 2
-    return count - 1
+        count += 1
+        if height > window:
+            count += 1
+    return count or -1
