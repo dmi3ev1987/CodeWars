943fe0ccd4c39c30346a35018b950c63ac72430c
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2025-07-12 08:08:47 +0000

    Refactor. Bouncing Balls.

--- 6-kyu/bouncing-balls/bouncing-balls copy.py
+++
@@ -1,8 +0,0 @@
-def bouncing_ball(height, bounce, window):
-    if height < 0 or bounce < 0 or bounce >= 1 or window >= height:
-        return -1
-    count = 0
-    while height > window:
-        height *= bounce
-        count += 2
-    return count - 1
