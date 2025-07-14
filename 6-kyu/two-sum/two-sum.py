626f6c129d2eb69c3fa8cebbd2e0ec2d70843a04
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-07-14 11:54:35 +0300

    Update solution - CodeHub

--- 6-kyu/two-sum/two-sum.py
+++ 6-kyu/two-sum/two-sum.py
@@ -1,10 +1,9 @@
 def two_sum(numbers, target):
-    result = None
     length = len(numbers)
     for index in range(length):
         for inner_index in range(length):
             if index == inner_index:
                 continue
             if numbers[index] + numbers[inner_index] == target:
-                result = (index, inner_index)
-    return result
+                return index, inner_index
+    return None
