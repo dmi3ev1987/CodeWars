74c7e844ae4da0f65adc11c1b492cea884ec7fd3
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-07-16 19:41:03 +0300

    Add solution - CodeHub

---
+++ 6-kyu/consecutive-strings/consecutive-strings.py
@@ -0,0 +1,14 @@
+def longest_consec(strarr, k):
+    if not strarr or k <= 0 or k > len(strarr):
+        return ""
+    
+    max_length = 0
+    result = ""
+    
+    for i in range(len(strarr) - k + 1):
+        current = ''.join(strarr[i:i+k])
+        if len(current) > max_length:
+            max_length = len(current)
+            result = current
+    
+    return result
