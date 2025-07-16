f1989374c290fe55c32f18aa7b80dae7b787ea11
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2025-07-16 16:41:52 +0000

    Consecutive strings.

--- 6-kyu/consecutive-strings/consecutive-strings.py
+++ 6-kyu/consecutive-strings/consecutive-strings.py
@@ -1,14 +1,14 @@
 def longest_consec(strarr, k):
     if not strarr or k <= 0 or k > len(strarr):
-        return ""
-    
+        return ''
+
     max_length = 0
-    result = ""
-    
+    result = ''
+
     for i in range(len(strarr) - k + 1):
-        current = ''.join(strarr[i:i+k])
+        current = ''.join(strarr[i : i + k])
         if len(current) > max_length:
             max_length = len(current)
             result = current
-    
+
     return result
