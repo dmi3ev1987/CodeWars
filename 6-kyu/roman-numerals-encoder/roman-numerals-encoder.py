a295343708c07e45814dcec34a11f8017c7059ef
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-07-22 11:39:55 +0300

    Add solution - CodeHub

---
+++ 6-kyu/roman-numerals-encoder/roman-numerals-encoder.py
@@ -0,0 +1,44 @@
+def solution(number):
+    SYMBOLS = {
+        'units': {1: 'I', 4: 'IV', 5: 'V', 9: 'IX'},
+        'tens': {1: 'X', 4: 'XL', 5: 'L', 9: 'XC'},
+        'hundreds': {1: 'C', 4: 'CD', 5: 'D', 9: 'CM'},
+        'thousands': {1: 'M'}
+    }
+    result = ''
+​
+    if not number or number < 1 or number > 3999:
+        return result
+       
+    thousands = number // 1000
+    if thousands:
+        result += SYMBOLS['thousands'][1] * thousands
+    
+    hundreds = (number % 1000) // 100
+    if hundreds:
+        if hundreds in SYMBOLS['hundreds']:
+            result += SYMBOLS['hundreds'][hundreds]
+        elif hundreds < 4:
+            result += SYMBOLS['hundreds'][1] * hundreds
+        else:
+            result += SYMBOLS['hundreds'][5] + SYMBOLS['hundreds'][1] * (hundreds - 5)
+    
+    tens = (number % 100) // 10
+    if tens:
+        if tens in SYMBOLS['tens']:
+            result += SYMBOLS['tens'][tens]
+        elif tens < 4:
+            result += SYMBOLS['tens'][1] * tens
+        else:
+            result += SYMBOLS['tens'][5] + SYMBOLS['tens'][1] * (tens - 5)
+    
+    units = number % 10
+    if units:
+        if units in SYMBOLS['units']:
+            result += SYMBOLS['units'][units]
+        elif units < 4:
+            result += SYMBOLS['units'][1] * units
+        else:
+            result += SYMBOLS['units'][5] + SYMBOLS['units'][1] * (units - 5)
+    
+    return result
