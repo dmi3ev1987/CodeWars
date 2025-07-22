4bb90b170f5cfaa7a0bfe4ae974964829c64a633
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2025-07-22 08:46:02 +0000

    Brute force. Roman Numerals Encoder.

--- 6-kyu/roman-numerals-encoder/roman-numerals-encoder.py
+++ 6-kyu/roman-numerals-encoder/roman-numerals-encoder.py
@@ -4,18 +4,18 @@ SYMBOLS = {
     'hundreds': {1: 'C', 4: 'CD', 5: 'D', 9: 'CM'},
     'thousands': {1: 'M'},
 }
-​
-​
+
+
 def solution(number):
     result = ''
-​
+
     if not number or number < 1 or number > 3999:
         return result
-​
+
     thousands = number // 1000
     if thousands:
         result += SYMBOLS['thousands'][1] * thousands
-​
+
     hundreds = (number % 1000) // 100
     if hundreds:
         if hundreds in SYMBOLS['hundreds']:
@@ -26,7 +26,7 @@ def solution(number):
             result += SYMBOLS['hundreds'][5] + SYMBOLS['hundreds'][1] * (
                 hundreds - 5
             )
-​
+
     tens = (number % 100) // 10
     if tens:
         if tens in SYMBOLS['tens']:
@@ -35,7 +35,7 @@ def solution(number):
             result += SYMBOLS['tens'][1] * tens
         else:
             result += SYMBOLS['tens'][5] + SYMBOLS['tens'][1] * (tens - 5)
-​
+
     units = number % 10
     if units:
         if units in SYMBOLS['units']:
@@ -44,6 +44,5 @@ def solution(number):
             result += SYMBOLS['units'][1] * units
         else:
             result += SYMBOLS['units'][5] + SYMBOLS['units'][1] * (units - 5)
-​
+
     return result
-​
