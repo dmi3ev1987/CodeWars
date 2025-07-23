13a19fb8add0d2b43b6dceddbb9c310ce284298d
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-07-23 15:41:29 +0300

    Update solution - CodeHub

--- 6-kyu/take-a-number-and-sum-its-digits-raised-to-the-consecutive-powers-and-dot-dot-dot-eureka/take-a-number-and-sum-its-digits-raised-to-the-consecutive-powers-and-dot-dot-dot-eureka.py
+++ 6-kyu/take-a-number-and-sum-its-digits-raised-to-the-consecutive-powers-and-dot-dot-dot-eureka/take-a-number-and-sum-its-digits-raised-to-the-consecutive-powers-and-dot-dot-dot-eureka.py
@@ -1,14 +1,10 @@
 def sum_dig_pow(a, b):
-    result = []
-    for number in range(a, b + 1):
-        if number < 10:
-            result.append(number)
-        else:
-            current_sum_of_digits = 0
-            count = 1
-            for digit in str(number):
-                current_sum_of_digits += int(digit) ** count
-                count += 1
-            if current_sum_of_digits == number:
-                result.append(number)
-    return result
+    return [
+        number
+        for number in range(a, b + 1)
+        if sum(
+            int(digit) ** index
+            for index, digit in enumerate(str(number), 1)
+        )
+        == number
+    ]
