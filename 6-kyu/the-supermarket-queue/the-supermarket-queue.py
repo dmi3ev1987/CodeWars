afb66881f00678598f4a8b4f0bbe8a4ccfce94d1
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-07-24 12:57:14 +0300

    Add solution - CodeHub

---
+++ 6-kyu/the-supermarket-queue/the-supermarket-queue.py
@@ -0,0 +1,11 @@
+def queue_time(customers, checkout_tills_number):
+    if checkout_tills_number == 1:
+        return sum(customers)
+    if checkout_tills_number >= len(customers):
+        return max(customers, default=0)
+​
+    tills = [0] * checkout_tills_number
+    for customer in customers:
+        min_till = tills.index(min(tills))
+        tills[min_till] += customer
+    return max(tills)
