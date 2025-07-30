78a7a6c7f5cd4a4ac9c5b1d23cc80d11cf10a92a
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-07-30 10:51:19 +0300

    Update solution - CodeHub

--- 6-kyu/multiplication-table/multiplication-table.py
+++ 6-kyu/multiplication-table/multiplication-table.py
@@ -1,8 +1,5 @@
 def multiplication_table(size):
     return [
-        [
-            inner_number
-            for inner_number in range(number, (number * size) + 1, number)
-        ]
+        [inner_number * number for inner_number in range(1, size + 1)]
         for number in range(1, size + 1)
     ]
