9a34cfa20cfa72db8d84c3cd34d5ce8522c09a01
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2025-08-02 20:52:09 +0000

    Give me a Diamond.

--- 6-kyu/give-me-a-diamond/give-me-a-diamond.py
+++ 6-kyu/give-me-a-diamond/give-me-a-diamond.py
@@ -2,7 +2,7 @@ def diamond(size):
     if size <= 0 or size % 2 == 0:
         return None
     diamond_part = [
-       ' ' * ((size - diamond) // 2) + '*' * diamond + '\n'
+        ' ' * ((size - diamond) // 2) + '*' * diamond + '\n'
         for diamond in range(1, size + 1, 2)
     ]
     return ''.join(diamond_part + diamond_part[-2::-1])
