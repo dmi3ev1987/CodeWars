631a86f901d5fbd0341d0493613bca4f58a795dc
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-08-07 17:21:30 +0300

    Update solution - CodeHub

--- 6-kyu/good-vs-evil/good-vs-evil.py
+++ 6-kyu/good-vs-evil/good-vs-evil.py
@@ -1,12 +1,14 @@
 def good_vs_evil(good, evil):
     race_force_good = [1, 2, 3, 3, 4, 10]
     race_force_evil = [1, 2, 2, 2, 3, 5, 10]
-    sum_good = 0
-    sum_evil = 0
-    for index, unit in enumerate(good.split()):
-        sum_good += int(unit) * race_force_good[index]
-    for index, unit in enumerate(evil.split()):
-        sum_evil += int(unit) * race_force_evil[index]
+    sum_good = sum(
+        int(unit) * race_force_good[index]
+        for index, unit in enumerate(good.split())
+    )
+    sum_evil = sum(
+        int(unit) * race_force_evil[index]
+        for index, unit in enumerate(evil.split())
+    )
     if sum_good == sum_evil:
         return 'Battle Result: No victor on this battle field'
     if sum_good > sum_evil:
