c32b40db6485ecac3286423c022e91fba980158b
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2025-08-13 09:31:36 +0000

    Brute force.

---
+++ 6-kyu/encrypt-this/brute_force_encrypt-this.py
@@ -0,0 +1,11 @@
+def encrypt_this(text):
+    result = []
+    for word in text.split():
+        if len(word) > 2:
+            encrypted_word = (
+                str(ord(word[:1])) + word[-1] + word[2:-1] + word[1]
+            )
+        else:
+            encrypted_word = str(ord(word[:1])) + word[1:]
+        result.append(encrypted_word)
+    return ' '.join(result)
