d77337357c5607a4250c71979b24250c83d40688
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-08-13 12:36:39 +0300

    Update solution - CodeHub

--- 6-kyu/encrypt-this/encrypt-this.py
+++ 6-kyu/encrypt-this/encrypt-this.py
@@ -1,21 +1,7 @@
 def encrypt_this(text):
-    encrypted_words = []
-    for word in text.split():
-        if not word:  # handle empty strings if any
-            continue
-
-        first_char = word[0]
-        encrypted = str(ord(first_char))
-
-        if len(word) > 1:
-            second_char = word[1]
-            last_char = word[-1]
-
-            if len(word) > 2:
-                middle = word[2:-1]
-                encrypted += last_char + middle + second_char
-            else:
-                encrypted += last_char
-
-        encrypted_words.append(encrypted)
-    return ' '.join(encrypted_words)
+    return ' '.join(
+        str(ord(word[:1])) + word[-1] + word[2:-1] + word[1]
+        if len(word) > 2
+        else str(ord(word[:1])) + word[1:]
+        for word in text.split()
+    )
