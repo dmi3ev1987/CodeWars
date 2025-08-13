660575eb709d3a3b0b862de6a4459fe6cae74d02
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2025-08-13 09:30:58 +0000

    Ruff format.

--- 6-kyu/encrypt-this/encrypt-this.py
+++ 6-kyu/encrypt-this/encrypt-this.py
@@ -2,7 +2,9 @@ def encrypt_this(text):
     result = []
     for word in text.split():
         if len(word) > 2:
-            encrypted_word = str(ord(word[:1])) + word[-1] + word[2:-1]+ word[1]
+            encrypted_word = (
+                str(ord(word[:1])) + word[-1] + word[2:-1] + word[1]
+            )
         else:
             encrypted_word = str(ord(word[:1])) + word[1:]
         result.append(encrypted_word)
