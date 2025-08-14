e5776170d394f145b3f73c5f8ed2e6fe7b92b35d
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2025-08-14 10:45:44 +0000

    The Vowel Code.

--- 6-kyu/the-vowel-code/the-vowel-code.py
+++ 6-kyu/the-vowel-code/the-vowel-code.py
@@ -1,9 +1,10 @@
 VOWELS = 'aeiou'
 CIPHER = '12345'
-​
+
+
 def encode(input_string):
     return input_string.translate(str.maketrans(VOWELS, CIPHER))
-    
-    
+
+
 def decode(input_string):
     return input_string.translate(str.maketrans(CIPHER, VOWELS))
