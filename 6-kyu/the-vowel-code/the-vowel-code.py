5a9dd4051a1fb258d1d6761f4a5aab9ebb20bf9d
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-08-14 13:45:02 +0300

    Update solution - CodeHub

--- 6-kyu/the-vowel-code/the-vowel-code.py
+++ 6-kyu/the-vowel-code/the-vowel-code.py
@@ -1,23 +1,9 @@
-PATTERN = {
-    'a': '1',
-    'e': '2',
-    'i': '3',
-    'o': '4',
-    'u': '5',
-    '1': 'a',
-    '2': 'e',
-    '3': 'i',
-    '4': 'o',
-    '5': 'u',
-}
-
-
+VOWELS = 'aeiou'
+CIPHER = '12345'
+​
 def encode(input_string):
-    return ''.join(
-        PATTERN[letter] if letter in PATTERN else letter
-        for letter in input_string
-    )
-
-
+    return input_string.translate(str.maketrans(VOWELS, CIPHER))
+    
+    
 def decode(input_string):
-    return encode(input_string)
+    return input_string.translate(str.maketrans(CIPHER, VOWELS))
