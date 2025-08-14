73b794b7d122d2aa35344b00b9919c467dbfc4fc
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2025-08-14 10:40:55 +0000

    Brute force.

---
+++ 6-kyu/the-vowel-code/brute_force_the-vowel-code.py
@@ -0,0 +1,23 @@
+PATTERN = {
+    'a': '1',
+    'e': '2',
+    'i': '3',
+    'o': '4',
+    'u': '5',
+    '1': 'a',
+    '2': 'e',
+    '3': 'i',
+    '4': 'o',
+    '5': 'u',
+}
+
+
+def encode(input_string):
+    return ''.join(
+        PATTERN[letter] if letter in PATTERN else letter
+        for letter in input_string
+    )
+
+
+def decode(input_string):
+    return encode(input_string)
