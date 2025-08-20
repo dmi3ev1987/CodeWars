a70ac5ce29aa2c9894490a6ba88ec8ee20baaf7b
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-08-20 09:01:13 +0300

    Update solution - CodeHub

--- 6-kyu/n-th-fibonacci/n-th-fibonacci.py
+++ 6-kyu/n-th-fibonacci/n-th-fibonacci.py
@@ -1,7 +1,5 @@
 def nth_fib(n):
-    fibo = [0, 1]
-    if n < 3:
-        return fibo[n - 1]
-    for index in range(1, n - 1):
-        fibo.append(fibo[index - 1] + fibo[index])
-    return fibo[-1]
+    a, b = 0, 1
+    for _ in range(n - 1):
+        a, b = b, a + b
+    return a
