a142cde34ffd1ec7c4fcf5fbcd57adb1354faff4
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2025-08-26 08:03:48 +0000

    Consonant value.

--- 6-kyu/consonant-value/consonant-value.py
+++ 6-kyu/consonant-value/consonant-value.py
@@ -1,9 +1,9 @@
 import re
-​
-​
+
+
 def solve(input_string):
     """Split each individual vowel (a, e, i, o, u).
-​
+
     Square brackets [] in `re.split` make it a character class.
     Example: re.split('[aeiou]', 'hello world') splits at 'e' and 'o'.
     """
