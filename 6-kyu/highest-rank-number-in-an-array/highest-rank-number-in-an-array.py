46ad6a5f45e9b1d9eb6f0a7c0988a4b5ab55f7cf
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-09-01 10:27:27 +0300

    Add solution - CodeHub

---
+++ 6-kyu/highest-rank-number-in-an-array/highest-rank-number-in-an-array.py
@@ -0,0 +1,11 @@
+from collections import Counter
+​
+​
+def highest_rank(input_array):
+    """Tuple (counter[x], x) as the sort key.
+​
+    - First sort by frequency (highest frequency first).
+    - If frequencies are equal, sort by the key value (highest value first).
+    """
+    counter = Counter(input_array)
+    return max(counter, key=lambda x: (counter[x], x))
