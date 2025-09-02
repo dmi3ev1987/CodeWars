c79535aaaa900ed2a5bf9a98f48518bda1bb0a9f
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-09-02 10:40:27 +0300

    Update solution - CodeHub

--- 6-kyu/length-of-missing-array/length-of-missing-array.py
+++ 6-kyu/length-of-missing-array/length-of-missing-array.py
@@ -1,12 +1,13 @@
 def get_length_of_missing_array(array_of_arrays):
-    if not array_of_arrays or None in array_of_arrays:
-        return 0
-    lengths = [len(array) for array in sorted(array_of_arrays, key=len)]
-    if lengths[0] == 0:
+    lengths = [
+        len(array) if array is not None else 0
+        for array in array_of_arrays
+    ]
+    lengths.sort()
+    if not lengths or 0 in lengths:
         return 0
     result = lengths[0]
-    for length in lengths[1:]:
-        result += 1
-        if result != length:
-            break
-    return result
+    for index in range(len(lengths) - 1):
+        if lengths[index + 1] != lengths[index] + 1:
+            return lengths[index] + 1
+    return 0
