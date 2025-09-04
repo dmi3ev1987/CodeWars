d3cbee278978c4857d723caa57a976a0eecb59d5
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-09-04 10:46:15 +0300

    Update solution - CodeHub

--- 6-kyu/a-rule-of-divisibility-by-13/a-rule-of-divisibility-by-13.py
+++ 6-kyu/a-rule-of-divisibility-by-13/a-rule-of-divisibility-by-13.py
@@ -1,13 +1,9 @@
 def thirt(input_number):
     pattern = [1, 10, 9, 12, 3, 4]
-    str_number = str(input_number)[::-1]
-    result = 0
-    patter_index = 0
-    for number in str_number:
-        result += int(number) * pattern[patter_index]
-        patter_index += 1
-        if patter_index == len(pattern):
-            patter_index = 0
-    if input_number != result:
-        return thirt(result)
-    return result
+    result = sum(
+        int(number) * pattern[index % 6]
+        for index, number in enumerate(reversed(str(input_number)))
+    )
+    if result == input_number:
+        return result
+    return thirt(result)
