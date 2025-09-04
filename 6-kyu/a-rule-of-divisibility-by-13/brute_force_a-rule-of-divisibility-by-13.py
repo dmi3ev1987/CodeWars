76ee05f0fa737ac70146c03b16499fa89f9dbb14
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2025-09-04 07:52:08 +0000

    Missprint fix.

--- 6-kyu/a-rule-of-divisibility-by-13/brute_force_a-rule-of-divisibility-by-13.py
+++ 6-kyu/a-rule-of-divisibility-by-13/brute_force_a-rule-of-divisibility-by-13.py
@@ -2,12 +2,12 @@ def thirt(input_number):
     pattern = [1, 10, 9, 12, 3, 4]
     str_number = str(input_number)[::-1]
     result = 0
-    patter_index = 0
+    pattern_index = 0
     for number in str_number:
-        result += int(number) * pattern[patter_index]
-        patter_index += 1
-        if patter_index == len(pattern):
-            patter_index = 0
+        result += int(number) * pattern[pattern_index]
+        pattern_index += 1
+        if pattern_index == len(pattern):
+            pattern_index = 0
     if input_number != result:
         return thirt(result)
     return result
