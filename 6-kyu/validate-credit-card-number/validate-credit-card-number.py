aa99ca8ccb434ef8962dc0ea4b8cddd1bc3d3d81
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2025-09-05 14:58:14 +0000

    Validate Credit Card Number.

--- 6-kyu/validate-credit-card-number/validate-credit-card-number.py
+++ 6-kyu/validate-credit-card-number/validate-credit-card-number.py
@@ -1,13 +1,15 @@
 def validate(input_number):
     length = len(str(input_number))
     str_number = str(input_number)[::-1]
-​
+
     result = []
     for index in range(length):
         current_number = int(str_number[index])
         if index % 2 == 1:
             doubled_number = current_number * 2
-            result.append(doubled_number - 9 if doubled_number > 9 else doubled_number)
+            result.append(
+                doubled_number - 9 if doubled_number > 9 else doubled_number
+            )
         else:
             result.append(current_number)
     return sum(result) % 10 == 0
