bad21ead89fd96fef3ca159862e8009d8d6a2a4f
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-09-09 10:07:27 +0300

    Add solution - CodeHub

---
+++ 6-kyu/pair-of-gloves/pair-of-gloves.py
@@ -0,0 +1,10 @@
+from collections import Counter
+​
+​
+def number_of_pairs(gloves):
+    glove_counter = Counter(gloves)
+    result = 0
+    for glove in glove_counter:
+        if glove_counter[glove] >= 2:
+            result += glove_counter[glove] // 2
+    return result
