9ed115cb7e4ee4c3316f8341f43934af3f2aa49d
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2025-09-11 07:23:13 +0000

    Fibonacci, Tribonacci and friends.

--- 6-kyu/fibonacci-tribonacci-and-friends/fibonacci-tribonacci-and-friends.py
+++ 6-kyu/fibonacci-tribonacci-and-friends/fibonacci-tribonacci-and-friends.py
@@ -2,5 +2,5 @@ def xbonacci(signature, n):
     start_position = 0
     while len(signature) < n:
         signature.append(sum(signature[start_position:]))
-        start_position +=1
+        start_position += 1
     return signature[:n]
