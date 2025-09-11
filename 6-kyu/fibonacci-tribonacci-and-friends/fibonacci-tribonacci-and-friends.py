101869cafc8c10c97949e963c717cc602b8258f9
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-09-11 10:20:43 +0300

    Add solution - CodeHub

---
+++ 6-kyu/fibonacci-tribonacci-and-friends/fibonacci-tribonacci-and-friends.py
@@ -0,0 +1,6 @@
+def xbonacci(signature, n):
+    start_position = 0
+    while len(signature) < n:
+        signature.append(sum(signature[start_position:]))
+        start_position +=1
+    return signature[:n]
