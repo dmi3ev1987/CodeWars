9b413796c61b6f9125173e001289d1e0bfb68cd7
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-09-17 10:41:48 +0300

    Add solution - CodeHub

---
+++ 6-kyu/multi-tap-keypad-text-entry-on-an-old-mobile-phone/multi-tap-keypad-text-entry-on-an-old-mobile-phone.py
@@ -0,0 +1,20 @@
+from collections import Counter
+​
+KEYPAD = {
+    '1ADGJMPTW*# ': 1,
+    'BEHKNQUX0': 2,
+    'CFILORVY': 3,
+    'SZ234568': 4,
+    '79': 5,
+}
+​
+​
+def presses(phrase):
+    letter_count = Counter(phrase.upper())
+    result = 0
+    for letter, number in letter_count.items():
+        for button, value in KEYPAD.items():
+            if letter in button:
+                result += number * value
+                break
+    return result
