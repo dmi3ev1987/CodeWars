865de9e9ac98120ea0eb08990165d05306dd4b4d
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-09-22 11:22:01 +0300

    Update solution - CodeHub

--- 6-kyu/prefill-an-array/prefill-an-array.py
+++ 6-kyu/prefill-an-array/prefill-an-array.py
@@ -1,9 +1,5 @@
-def prefill(size_number, value='undefined'):
-    message = f'{size_number} is invalid'
+def prefill(size_number=0, value=None):
     try:
-        int_size_number = int(size_number)
+        return int(size_number) * [value]
     except (ValueError, TypeError):
-        raise TypeError(message)
-    if int_size_number < 0:
-        raise TypeError(message)
-    return int_size_number * [value]
+        raise TypeError(f'{size_number} is invalid')
