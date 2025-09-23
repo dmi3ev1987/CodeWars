5e91fe538028951ab8c73d0ebfad97978dd8689e
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2025-09-23 11:13:04 +0000

    Word a10n (abbreviation).

--- 6-kyu/word-a10n-abbreviation/word-a10n-abbreviation.py
+++ 6-kyu/word-a10n-abbreviation/word-a10n-abbreviation.py
@@ -6,7 +6,9 @@ def abbreviate(input_string):
             part_word += letter
         else:
             if len(part_word) > 3:
-                part_word = part_word[0] + str(len(part_word) - 2) + part_word[-1]
+                part_word = (
+                    part_word[0] + str(len(part_word) - 2) + part_word[-1]
+                )
             result += part_word + letter
             part_word = ''
     if len(part_word) > 3:
