1ef0cd871ad629b856fe4880381f315277dc0fa3
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-09-23 14:11:43 +0300

    Add solution - CodeHub

---
+++ 6-kyu/word-a10n-abbreviation/word-a10n-abbreviation.py
@@ -0,0 +1,15 @@
+def abbreviate(input_string):
+    result = ''
+    part_word = ''
+    for letter in input_string:
+        if letter.isalpha():
+            part_word += letter
+        else:
+            if len(part_word) > 3:
+                part_word = part_word[0] + str(len(part_word) - 2) + part_word[-1]
+            result += part_word + letter
+            part_word = ''
+    if len(part_word) > 3:
+        part_word = part_word[0] + str(len(part_word) - 2) + part_word[-1]
+    result += part_word
+    return result
