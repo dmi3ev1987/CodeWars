2474f03989e9000b955c4ec34ee95fae5fe459c8
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-09-25 17:50:07 +0300

    Update solution - CodeHub

--- 6-kyu/plus-1-array/plus-1-array.py
+++ 6-kyu/plus-1-array/plus-1-array.py
@@ -1,17 +1,15 @@
 def up_array(input_array):
-    if not input_array:
+    if not input_array or any(number > 9 or number < 0 for number in input_array):
         return None
     result = []
     value_to_add = 1
     for index in range(len(input_array) - 1, -1, -1):
-        if input_array[index] < 0 or input_array[index] > 9:
-            return None
         number_to_add = input_array[index] + value_to_add
-        if number_to_add == 10:
+        if number_to_add > 9:
             result.append(0)
         else:
             result.append(number_to_add)
             value_to_add = 0
-    if value_to_add != 0:
+    if value_to_add:
         result.append(value_to_add)
     return result[::-1]
