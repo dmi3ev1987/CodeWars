bc73263b62c75140f770b295475adce12d17d994
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-09-26 18:00:08 +0300

    Add solution - CodeHub

---
+++ 6-kyu/if-you-can-read-this-dot-dot-dot/if-you-can-read-this-dot-dot-dot.py
@@ -0,0 +1,9 @@
+from preloaded import NATO # NATO['A'] == 'Alfa', etc
+​
+def to_nato(words : str) -> str:
+    return ' '.join(
+        NATO[letter.upper()]
+        if letter.isalpha()
+        else ('' if letter == ' ' else letter)
+        for letter in words.strip()
+    ).replace('  ', ' ')
