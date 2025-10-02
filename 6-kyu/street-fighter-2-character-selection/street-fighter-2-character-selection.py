a7d08716c6563bf64d2825dfdb45ad2eecb89509
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-10-02 17:39:37 +0300

    Add solution - CodeHub

---
+++ 6-kyu/street-fighter-2-character-selection/street-fighter-2-character-selection.py
@@ -0,0 +1,16 @@
+def street_fighter_selection(fighters, initial_position, moves):
+    opts = ['up', 'down', 'right', 'left']
+    level, position = initial_position
+    result = []
+​
+    for move in moves:
+        if move == opts[0]:
+            level = 0
+        if move == opts[1]:
+            level = 1
+        if move == opts[2]:
+            position = (position + 1) % len(fighters[0])
+        if move == opts[3]:
+            position = (position - 1) % len(fighters[0])
+        result.append(fighters[level][position])
+    return result
