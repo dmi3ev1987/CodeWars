c79eee65e02b498b2dacf0e228a24c12699f10bd
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2025-10-31 14:35:13 +0000

    Power of two.

--- 7-kyu/power-of-two/power-of-two.py
+++ 7-kyu/power-of-two/power-of-two.py
@@ -2,6 +2,6 @@ def power_of_two(x):
     count = 0
     result = 0
     while result < x:
-        result = 2 ** count 
+        result = 2**count
         count += 1
     return result == x if x > 0 else False
