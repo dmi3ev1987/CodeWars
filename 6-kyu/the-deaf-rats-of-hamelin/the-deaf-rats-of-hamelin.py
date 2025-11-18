04c00654fc54be78f9f9b78ffb95bc7f4dd4a8eb
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-11-18 16:56:26 +0300

    Update solution - CodeHub

--- 6-kyu/the-deaf-rats-of-hamelin/the-deaf-rats-of-hamelin.py
+++ 6-kyu/the-deaf-rats-of-hamelin/the-deaf-rats-of-hamelin.py
@@ -1,11 +1,11 @@
 def count_deaf_rats(town):
     town = town.replace(' ', '')
     coax_list = town.split('P')
-    coax = list(coax_list[0][::-1] + coax_list[1])
+    coax = list(coax_list[0] + coax_list[1][::-1])
     deaf_count = 0
     while coax:
-        if coax[0] == '~':
+        if coax[-1] == '~':
             deaf_count += 1
-        coax.pop(0)
-        coax.pop(0)
+        coax.pop()
+        coax.pop()
     return deaf_count
