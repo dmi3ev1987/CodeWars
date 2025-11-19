68efcdd007224898da21a81ddf3fe863c0bab90e
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-11-19 16:51:32 +0300

    Add solution - CodeHub

---
+++ 6-kyu/coding-meetup-number-7-higher-order-functions-series-find-the-most-senior-developer/coding-meetup-number-7-higher-order-functions-series-find-the-most-senior-developer.py
@@ -0,0 +1,10 @@
+def find_senior(input_list): 
+    input_list.sort(key = lambda x: x['age'], reverse=True)
+    max_age = input_list[0]['age']
+    count = 0
+    for developer in input_list:
+        if developer['age'] == max_age:
+            count += 1
+        else:
+            break  
+    return input_list[:count]
