a3abaf1251763516717a3affb915c925b134f663
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2025-11-19 13:53:40 +0000

    Coding Meetup #7 - Higher-Order Functions Series - Find the most senior developer.

--- 6-kyu/coding-meetup-number-7-higher-order-functions-series-find-the-most-senior-developer/coding-meetup-number-7-higher-order-functions-series-find-the-most-senior-developer.py
+++ 6-kyu/coding-meetup-number-7-higher-order-functions-series-find-the-most-senior-developer/coding-meetup-number-7-higher-order-functions-series-find-the-most-senior-developer.py
@@ -1,10 +1,10 @@
-def find_senior(input_list): 
-    input_list.sort(key = lambda x: x['age'], reverse=True)
+def find_senior(input_list):
+    input_list.sort(key=lambda x: x['age'], reverse=True)
     max_age = input_list[0]['age']
     count = 0
     for developer in input_list:
         if developer['age'] == max_age:
             count += 1
         else:
-            break  
+            break
     return input_list[:count]
