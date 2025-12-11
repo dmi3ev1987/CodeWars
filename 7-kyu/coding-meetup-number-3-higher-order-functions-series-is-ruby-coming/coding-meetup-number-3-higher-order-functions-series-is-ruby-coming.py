818556c1eee63f02a2bafb434b33ecce964cd93d
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2025-12-11 05:20:06 +0000

    Ruff format.

--- 7-kyu/coding-meetup-number-3-higher-order-functions-series-is-ruby-coming/coding-meetup-number-3-higher-order-functions-series-is-ruby-coming.py
+++ 7-kyu/coding-meetup-number-3-higher-order-functions-series-is-ruby-coming/coding-meetup-number-3-higher-order-functions-series-is-ruby-coming.py
@@ -1,4 +1,4 @@
-def is_ruby_coming(developers_list): 
+def is_ruby_coming(developers_list):
     for developer in developers_list:
         if developer['language'] == 'Ruby':
             return True
