bea3ad171c57e8cca4c62a40aeda2496ce78d302
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2025-12-11 05:22:39 +0000

    Coding Meetup #3 - Higher-Order Functions Series - Is Ruby coming?

--- 7-kyu/coding-meetup-number-3-higher-order-functions-series-is-ruby-coming/coding-meetup-number-3-higher-order-functions-series-is-ruby-coming.py
+++ 7-kyu/coding-meetup-number-3-higher-order-functions-series-is-ruby-coming/coding-meetup-number-3-higher-order-functions-series-is-ruby-coming.py
@@ -1,2 +1,4 @@
 def is_ruby_coming(developers_list):
-    return any(developer['language'] == 'Ruby' for developer in developers_list)
+    return any(
+        developer['language'] == 'Ruby' for developer in developers_list
+    )
