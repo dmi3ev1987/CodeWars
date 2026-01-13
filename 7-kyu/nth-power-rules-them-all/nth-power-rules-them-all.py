c2250e7ffe9deef8c97d031940789ea53eb1ad2d
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2026-01-13 09:38:05 +0300

    Update solution - CodeHub

--- 7-kyu/nth-power-rules-them-all/nth-power-rules-them-all.py
+++ 7-kyu/nth-power-rules-them-all/nth-power-rules-them-all.py
@@ -1,4 +1,3 @@
 def modified_sum(input_array, increase_power):
-    return sum([number**increase_power for number in input_array]) - sum(
-        input_array
-    )
+    return sum([number**increase_power - number for number in input_array])
+​
