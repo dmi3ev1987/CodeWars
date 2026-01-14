ab2d0ef6903946fd9b938e6f0c5189bfee55498b
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2026-01-14 08:33:45 +0300

    Update solution - CodeHub

--- 7-kyu/fibonacci/fibonacci.py
+++ 7-kyu/fibonacci/fibonacci.py
@@ -1,6 +1,5 @@
 def fibonacci(fibo_number: int) -> int:
-    sequence = [0, 1, 1]
-    if fibo_number > 2:
-        for _ in range(2, fibo_number):
-            sequence.append(sequence[-1] + sequence[-2])
-    return sequence[fibo_number]
+    x, y = 0, 1
+    for _ in range(fibo_number):
+        x, y = y, x + y
+    return x
