ddf484441ac8d2c53b884ca1286e8e43287ddf28
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2026-01-15 05:57:26 +0000

    Double Every Other.

--- 7-kyu/double-every-other/double-every-other.py
+++ 7-kyu/double-every-other/double-every-other.py
@@ -1,7 +1,5 @@
 def double_every_other(input_list):
     return [
-        number * 2
-        if index % 2 != 0
-        else number
+        number * 2 if index % 2 != 0 else number
         for index, number in enumerate(input_list)
     ]
