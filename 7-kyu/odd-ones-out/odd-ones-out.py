6babea00504eef8ee9945771d91391830b10545f
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2026-01-21 09:47:40 +0300

    Add solution - CodeHub

---
+++ 7-kyu/odd-ones-out/odd-ones-out.py
@@ -0,0 +1,9 @@
+from collections import Counter
+​
+​
+def odd_ones_out(numbers):
+    counts = Counter(numbers)
+    return  [
+        number for number in numbers
+        if counts[number] % 2 == 0
+    ]
