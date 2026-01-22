6bba3e926aed9a1196a1f6335a6b3d285197b003
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2026-01-22 10:07:14 +0300

    Add solution - CodeHub

---
+++ 7-kyu/coding-meetup-number-14-higher-order-functions-series-order-the-food/coding-meetup-number-14-higher-order-functions-series-order-the-food.py
@@ -0,0 +1,8 @@
+from collections import defaultdict
+​
+​
+def order_food(developers):
+    food = defaultdict(int)
+    for developer in developers:
+        food[developer['meal']] += 1
+    return  food
