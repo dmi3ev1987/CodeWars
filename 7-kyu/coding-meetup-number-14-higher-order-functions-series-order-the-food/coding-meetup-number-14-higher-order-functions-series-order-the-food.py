61eafce434de3d08c69eff5cca243302cf25132c
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2026-01-22 07:17:44 +0000

    Coding Meetup #14 - Higher-Order Functions Series - Order the food.

--- 7-kyu/coding-meetup-number-14-higher-order-functions-series-order-the-food/coding-meetup-number-14-higher-order-functions-series-order-the-food.py
+++ 7-kyu/coding-meetup-number-14-higher-order-functions-series-order-the-food/coding-meetup-number-14-higher-order-functions-series-order-the-food.py
@@ -1,8 +1,8 @@
 from collections import defaultdict
-​
-​
+
+
 def order_food(developers):
     food = defaultdict(int)
     for developer in developers:
         food[developer['meal']] += 1
-    return  food
+    return food
