32bac45cbd1e311c59bdb3b80bf33125e5e299ef
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2026-01-30 17:23:05 +0300

    Update solution - CodeHub

--- 7-kyu/254-shades-of-grey/254-shades-of-grey.py
+++ 7-kyu/254-shades-of-grey/254-shades-of-grey.py
@@ -1,4 +1,5 @@
 def shades_of_grey(input_number):
-    if input_number > 254:
-        input_number = 254
-    return ['#' + format(number, '02x') * 3 for number in range(1, input_number + 1)]
+    return [
+        '#' + format(number, '02x') * 3
+        for number in range(1, min(254, input_number) + 1)
+    ]
