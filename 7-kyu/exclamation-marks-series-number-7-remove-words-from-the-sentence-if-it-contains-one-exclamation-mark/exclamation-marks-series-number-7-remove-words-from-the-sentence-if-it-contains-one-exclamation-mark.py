2a0df9c0f0577d2d7f0c3083657054167360ed27
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2026-02-02 08:23:29 +0000

    Exclamation marks series #7: Remove words from the sentence if it contains one exclamation mark.

--- 7-kyu/exclamation-marks-series-number-7-remove-words-from-the-sentence-if-it-contains-one-exclamation-mark/exclamation-marks-series-number-7-remove-words-from-the-sentence-if-it-contains-one-exclamation-mark.py
+++ 7-kyu/exclamation-marks-series-number-7-remove-words-from-the-sentence-if-it-contains-one-exclamation-mark/exclamation-marks-series-number-7-remove-words-from-the-sentence-if-it-contains-one-exclamation-mark.py
@@ -1,2 +1,4 @@
 def remove(input_string):
-    return ' '.join(word for word in input_string.split() if word.count('!') != 1)
+    return ' '.join(
+        word for word in input_string.split() if word.count('!') != 1
+    )
