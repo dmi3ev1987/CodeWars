88532dd31e04d8fb44c2da9e46572983b24feff1
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2026-02-03 06:21:52 +0000

    Sort the Vowels!

--- 7-kyu/sort-the-vowels/sort-the-vowels.py
+++ 7-kyu/sort-the-vowels/sort-the-vowels.py
@@ -2,8 +2,6 @@ def sort_vowels(input_string):
     if not isinstance(input_string, str):
         return ''
     return '\n'.join(
-        '|' + char
-        if char.lower() in 'aeiou'
-        else char + '|'
+        '|' + char if char.lower() in 'aeiou' else char + '|'
         for char in input_string
-        )
+    )
