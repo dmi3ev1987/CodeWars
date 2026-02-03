394e1fe55a7e974091fe112647388e455bfbf6c6
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2026-02-03 09:23:22 +0300

    Update solution - CodeHub

--- 7-kyu/sort-the-vowels/sort-the-vowels.py
+++ 7-kyu/sort-the-vowels/sort-the-vowels.py
@@ -1,7 +1,9 @@
 def sort_vowels(input_string):
-    if not isinstance(input_string, str):
-        return ''
-    return '\n'.join(
-        '|' + char if char.lower() in 'aeiou' else char + '|'
-        for char in input_string
+    return (
+        ''
+        if not isinstance(input_string, str)
+        else '\n'.join(
+            '|' + char if char.lower() in 'aeiou' else char + '|'
+            for char in input_string
+        )
     )
