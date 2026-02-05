87570e5e602a759a5ab0a1400aefa349ba30e5d3
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2026-02-05 11:22:37 +0300

    Add solution - CodeHub

---
+++ 7-kyu/next-palindromic-number/next-palindromic-number.py
@@ -0,0 +1,7 @@
+def next_pal(input_number):
+    while True:
+        input_number += 1
+        half_number_length = len(str(input_number)) // 2
+        if str(input_number)[:half_number_length] == str(input_number)[-half_number_length:][::-1]:
+            break
+    return input_number
