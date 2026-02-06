f386c5eafb0877e84978b127fa7f0d28853722a4
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2026-02-06 09:39:39 +0300

    Update solution - CodeHub

--- 7-kyu/change-two-dimensional-array/change-two-dimensional-array.py
+++ 7-kyu/change-two-dimensional-array/change-two-dimensional-array.py
@@ -1,12 +1,4 @@
 def matrix(input_array):
-    result_matrix = []
-    counter = 0
-    for inner_array in input_array:
-        number_to_add = 0 if inner_array[counter] < 0 else 1
-        result_matrix.append(
-            inner_array[:counter]
-            + [number_to_add]
-            + inner_array[counter + 1 :]
-        )
-        counter += 1
-    return result_matrix
+    for index in range(len(input_array)):
+        input_array[index][index] = 0 if input_array[index][index] < 0 else 1
+    return input_array
