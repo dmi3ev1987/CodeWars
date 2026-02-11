2b879f241a2bc7f4f4ec7c5be69d7ca4e4b711bd
andrey.dmi3ev <andrey.dmi3ev@gmail.com>
2026-02-11 08:56:58 +0000

    The First Non Repeated Character In A String.

--- 7-kyu/the-first-non-repeated-character-in-a-string/the-first-non-repeated-character-in-a-string.py
+++ 7-kyu/the-first-non-repeated-character-in-a-string/the-first-non-repeated-character-in-a-string.py
@@ -1,8 +1,10 @@
 from collections import Counter
-​
-​
+
+
 def first_non_repeated(input_string):
-    single_chars = [key for key, value in Counter(input_string).items() if value == 1]
+    single_chars = [
+        key for key, value in Counter(input_string).items() if value == 1
+    ]
     first_char = None
     for char in input_string:
         if char in single_chars:
