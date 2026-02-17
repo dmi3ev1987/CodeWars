5d54e0b3d45ced410d3fbc070163a2926c63a2d8
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2026-02-17 12:03:10 +0300

    Update solution - CodeHub

--- 7-kyu/mod-256-without-the-mod-operator/mod-256-without-the-mod-operator.py
+++ 7-kyu/mod-256-without-the-mod-operator/mod-256-without-the-mod-operator.py
@@ -1,18 +1,8 @@
 def mod256_without_mod(input_number):
     mod_number = 256
-    if abs(input_number) == mod_number:
-        return 0
-    if input_number > mod_number:
-        result = int(input_number / mod_number)
-        return input_number - (mod_number * result)
-    elif input_number < 0:
-        if -input_number < mod_number:
-            return input_number + mod_number
-        else:
-            return (
-                input_number
-                - (int(input_number / mod_number)) * mod_number
-                + mod_number
-            )
-    else:
-        return input_number
+    result = input_number
+    while result >= mod_number:
+        result -= mod_number
+    while result < 0:
+        result += mod_number
+    return result
