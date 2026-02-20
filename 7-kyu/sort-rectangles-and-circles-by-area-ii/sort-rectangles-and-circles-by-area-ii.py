3b58917c239981a463f1880bb0b13a12d2cdab0b
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2026-02-20 17:21:11 +0300

    Update solution - CodeHub

--- 7-kyu/sort-rectangles-and-circles-by-area-ii/sort-rectangles-and-circles-by-area-ii.py
+++ 7-kyu/sort-rectangles-and-circles-by-area-ii/sort-rectangles-and-circles-by-area-ii.py
@@ -1,17 +1,7 @@
 from math import pi
-
-
+​
+​
 def sort_by_area(sequence):
-    result = []
-    index = 0
-    squares = []
-    for area in sequence:
-        if type(area) == tuple:
-            square = area[0] * area[1]
-        else:
-            square = pi * area**2
-        squares.append((index, square))
-        index += 1
-    for sorted_area in sorted(squares, key=lambda x: x[1]):
-        result.append(sequence[sorted_area[0]])
-    return result
+    return sorted(
+        sequence, key=lambda x: x[0] * x[1] if type(x) == tuple else pi * x**2
+    )
