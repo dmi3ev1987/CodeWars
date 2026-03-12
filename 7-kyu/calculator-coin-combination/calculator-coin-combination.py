0824fda6ceef8caa6ef2c9b9acab5b3f8214da94
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2026-03-12 17:59:16 +0300

    Add solution - CodeHub

---
+++ 7-kyu/calculator-coin-combination/calculator-coin-combination.py
@@ -0,0 +1,15 @@
+def coin_combo(cents):
+    result = [0, 0, 0, 0]
+    while cents >= 25:
+        cents -= 25
+        result[3] += 1
+    while cents >= 10:
+        cents -= 10
+        result[2] += 1
+    while cents >= 5:
+        cents -= 5
+        result[1] += 1
+    while cents >= 1:
+        cents -= 1
+        result[0] += 1       
+    return result
