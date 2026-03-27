b769f0cc5bb720a8770f81ea94be84dfdf322784
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2026-03-27 17:05:27 +0300

    Add solution - CodeHub

---
+++ 7-kyu/fibonaccis-fizzbuzz/fibonaccis-fizzbuzz.py
@@ -0,0 +1,14 @@
+def fibs_fizz_buzz(n):
+    a, b = 1, 1
+    result = [a, b]
+    for _ in range(n):
+        a, b = a + b, a
+        result.append(a)
+        if result[-1] % 3 == 0 and result[-1] % 5 == 0:
+            result[-1] = 'FizzBuzz'
+        elif result[-1] % 3 == 0:
+            result[-1] = 'Fizz'
+        elif result[-1] % 5 == 0:
+            result[-1] = 'Buzz'
+    return result[:n]
+​
