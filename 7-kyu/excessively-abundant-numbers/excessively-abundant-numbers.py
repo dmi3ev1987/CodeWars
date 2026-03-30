69c7c747a447719370793f05a7832fc82978857e
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2026-03-30 15:57:25 +0300

    Add solution - CodeHub

---
+++ 7-kyu/excessively-abundant-numbers/excessively-abundant-numbers.py
@@ -0,0 +1,6 @@
+def abundant_number(num):
+    devisors = []
+    for current_num in range(1, num):
+        if num % current_num == 0:
+            devisors.append(current_num)
+    return True if sum(devisors) > num else False
