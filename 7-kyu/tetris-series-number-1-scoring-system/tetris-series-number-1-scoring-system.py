7e3f944f210d096d313f0e76f78d44de82be4ec5
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2026-03-31 11:55:31 +0000

    Tetris Series #1 — Scoring System.

--- 7-kyu/tetris-series-number-1-scoring-system/tetris-series-number-1-scoring-system.py
+++ 7-kyu/tetris-series-number-1-scoring-system/tetris-series-number-1-scoring-system.py
@@ -3,7 +3,7 @@ def get_score(arr) -> int:
     lines_count = 0
     total_score = 0
     points_system = [40, 100, 300, 1200]
-    
+
     for line in arr:
         if line > 0:
             total_score += points_system[line - 1] * (lvl + 1)
@@ -11,5 +11,5 @@ def get_score(arr) -> int:
         if lines_count > 9:
             lvl += 1
             lines_count %= 10
-​
+
     return total_score
