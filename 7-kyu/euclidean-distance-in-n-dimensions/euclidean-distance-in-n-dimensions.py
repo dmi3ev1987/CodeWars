3a6c45e087f0fa0000fb7b00533bb08479462218
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2026-04-01 16:40:08 +0300

    Add solution - CodeHub

---
+++ 7-kyu/euclidean-distance-in-n-dimensions/euclidean-distance-in-n-dimensions.py
@@ -0,0 +1,9 @@
+import math
+​
+def euclidean_distance(point1, point2):
+    sum_squared = 0
+    for i in range(len(point1)):
+        sum_squared += (point1[i] - point2[i]) ** 2
+    
+    distance = math.sqrt(sum_squared)
+    return round(distance, 2)
