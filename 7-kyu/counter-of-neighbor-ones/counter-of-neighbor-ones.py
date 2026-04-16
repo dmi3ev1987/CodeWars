cc4edd19325532b87eac331b782eac518cf1172d
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2026-04-16 16:44:40 +0300

    Update solution - CodeHub

--- 7-kyu/counter-of-neighbor-ones/counter-of-neighbor-ones.py
+++ 7-kyu/counter-of-neighbor-ones/counter-of-neighbor-ones.py
@@ -2,10 +2,11 @@ def ones_counter(arr):
     count = 0
     result = []
     for number in arr:
-        if number == 0:
+        if number == 0 and count != 0:
             result.append(count)
             count = 0
         else:
             count += number
-    result.append(count)
-    return [num for num in result if num != 0]
+    if count != 0:
+        result.append(count)
+    return result
