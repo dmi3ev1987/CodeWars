bc368e74ea722042b1b043341514b3dfb644d393
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2026-04-21 16:17:12 +0300

    Update solution - CodeHub

--- 7-kyu/sort-deck-of-cards/sort-deck-of-cards.py
+++ 7-kyu/sort-deck-of-cards/sort-deck-of-cards.py
@@ -1,3 +1,3 @@
 def sort_cards(cards):
-    pattern = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K']
-    return sorted(cards, key=pattern.index)
+    pattern = {'A': 0, '2': 1, '3': 2, '4': 3, '5': 4, '6': 5, '7': 6, '8': 7, '9': 8, 'T': 9, 'J': 10, 'Q': 11, 'K': 12}
+    return sorted(cards, key=lambda card: pattern[card])
