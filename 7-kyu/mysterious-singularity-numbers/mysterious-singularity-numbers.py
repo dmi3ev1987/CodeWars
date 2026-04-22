ac02b76eaedee179590444d347d1e0f21428c615
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2026-04-22 13:09:44 +0000

    Mysterious Singularity Numbers.

--- 7-kyu/mysterious-singularity-numbers/mysterious-singularity-numbers.py
+++ 7-kyu/mysterious-singularity-numbers/mysterious-singularity-numbers.py
@@ -1,2 +1,4 @@
 def real_numbers(n):
-    return n - (n//2 + n//3 + n//5) + (n//6 + n//10 + n//15) - n//30
+    return (
+        n - (n // 2 + n // 3 + n // 5) + (n // 6 + n // 10 + n // 15) - n // 30
+    )
