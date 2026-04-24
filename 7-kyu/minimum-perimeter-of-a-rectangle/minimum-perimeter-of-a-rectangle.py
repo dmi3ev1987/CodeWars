21df937f93131eef1171aec63fcf9b449aa5f9fd
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2026-04-24 12:25:59 +0000

    Minimum Perimeter of a Rectangle.

--- 7-kyu/minimum-perimeter-of-a-rectangle/minimum-perimeter-of-a-rectangle.py
+++ 7-kyu/minimum-perimeter-of-a-rectangle/minimum-perimeter-of-a-rectangle.py
@@ -1,6 +1,6 @@
 def minimum_perimeter(area):
     min_per = 2 * (1 + area)
-    for width in range(1, int(area ** 0.5) + 1):
+    for width in range(1, int(area**0.5) + 1):
         if area % width == 0:
             height = area // width
             min_per = min(min_per, 2 * (width + height))
